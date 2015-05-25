257323fa78aee6adf2f563a3ab8fb4e57d85ee26
Chris Done <chrisdone@gmail.com>
2015-05-25 21:24:13 +0200

    Proper GhcPkgId parser

--- src/Stack/Types/GhcPkgId.hs
+++ src/Stack/Types/GhcPkgId.hs
@@ -6,36 +6,74 @@
 
 module Stack.Types.GhcPkgId
   (GhcPkgId
-  ,parseGhcPkgIdFromString)
+  ,ghcPkgIdParser
+  ,parseGhcPkgId
+  ,ghcPkgIdString
+  ,ghcPkgIdPackageIdentifier)
   where
 
+import           Control.Applicative
 import           Control.Monad.Catch
 import           Data.Aeson
-import           Data.ByteString (ByteString)
+import           Data.Attoparsec.ByteString.Char8
+import           Data.ByteString.Char8 (ByteString)
 import qualified Data.ByteString.Char8 as S8
+import           Data.Char (isLetter)
 import           Data.Data
 import           Data.Hashable
+import           Data.Text.Encoding (encodeUtf8)
 import           GHC.Generics
+import           Stack.Types.PackageIdentifier
+
+-- | A parse fail.
+data GhcPkgIdParseFail
+  = GhcPkgIdParseFail ByteString
+  deriving (Show,Typeable)
+instance Exception GhcPkgIdParseFail
 
 -- | A ghc-pkg package identifier.
-newtype GhcPkgId = GhcPkgId ByteString
+data GhcPkgId =
+  GhcPkgId !PackageIdentifier
+           !ByteString
   deriving (Eq,Ord,Data,Typeable,Generic)
 
 instance Hashable GhcPkgId
 
 instance Show GhcPkgId where
-  show (GhcPkgId x) = S8.unpack x
+  show = show . ghcPkgIdString
 
 instance FromJSON GhcPkgId where
-  parseJSON x =
-    do s <- parseJSON x
-       let !b = S8.pack s
-       return (GhcPkgId b)
+  parseJSON = withText "GhcPkgId" $ \t ->
+    case parseGhcPkgId $ encodeUtf8 t of
+      Left e -> fail $ show (e, t)
+      Right x -> return x
 
 instance ToJSON GhcPkgId where
-  toJSON (GhcPkgId x) =
-    toJSON (S8.unpack x)
+  toJSON g =
+    toJSON (ghcPkgIdString g)
 
--- | Parse a GHC package id from a string.
-parseGhcPkgIdFromString :: MonadThrow m => String -> m GhcPkgId
-parseGhcPkgIdFromString = return . GhcPkgId . S8.pack
+-- | Convenient way to parse a package name from a bytestring.
+parseGhcPkgId :: MonadThrow m => ByteString -> m GhcPkgId
+parseGhcPkgId x = go x
+  where go =
+          either (const (throwM (GhcPkgIdParseFail x))) return .
+          parseOnly (ghcPkgIdParser <* endOfInput)
+
+-- | A parser for a package-version-hash pair.
+ghcPkgIdParser :: Parser GhcPkgId
+ghcPkgIdParser =
+  do ident <- packageIdentifierParser
+     _ <- char8 '-'
+     h <- many1 (satisfy isAlphaNum)
+     let !bytes = S8.pack h
+     return (GhcPkgId ident bytes)
+  where isAlphaNum c = isLetter c || isDigit c
+
+-- | Get a string representation of GHC package id.
+ghcPkgIdString :: GhcPkgId -> String
+ghcPkgIdString (GhcPkgId ident x) =
+  packageIdentifierString ident ++ "-" ++ S8.unpack x
+
+-- | Get the package identifier of the GHC package id.
+ghcPkgIdPackageIdentifier :: GhcPkgId -> PackageIdentifier
+ghcPkgIdPackageIdentifier (GhcPkgId ident _) = ident
