09b4f57a8e679b186c86012a0da8229be8d70dfb
Chris Done <chrisdone@gmail.com>
2015-05-27 19:57:42 +0200

    Update .dir-locals.el/.ghci for stack exec ghci

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,5 @@
 ((haskell-mode . ((haskell-indent-spaces . 4)
-                  (hindent-style . "johan-tibell"))))
+                  (hindent-style . "johan-tibell")
+                  (haskell-process-type . ghci)
+                  (haskell-process-path-ghci . "stack")
+                  (haskell-process-args-ghci . ("exec" "ghci-ng")))))
