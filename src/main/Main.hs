a49a41c269b8f29ca307cf0076b394de0f7ec0f5
Michael Snoyman <michael@snoyman.com>
2015-05-27 15:07:56 +0300

    Add ghc, ghci, and runghc commands

--- src/main/Main.hs
+++ src/main/Main.hs
@@ -71,6 +71,24 @@ main =
                         ((,)
                             <$> strArgument (metavar "CMD")
                             <*> many (strArgument (metavar "[ARGS]")))
+             addCommand "ghc"
+                        "Run ghc"
+                        execCmd
+                        ((,)
+                            <$> pure "ghc"
+                            <*> many (strArgument (metavar "[ARGS]")))
+             addCommand "ghci"
+                        "Run ghci"
+                        execCmd
+                        ((,)
+                            <$> pure "ghci"
+                            <*> many (strArgument (metavar "[ARGS]")))
+             addCommand "runghc"
+                        "Run runghc"
+                        execCmd
+                        ((,)
+                            <$> pure "runghc"
+                            <*> many (strArgument (metavar "[ARGS]")))
              addCommand "clean"
                         "Clean the local packages"
                         cleanCmd
