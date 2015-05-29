f7405d801e31ad6cb93ef5a4f7ee055ce2330a43
Michael Snoyman <michael@snoyman.com>
2015-05-29 08:04:48 +0300

    Fix incorrect exception text #110

--- src/main/Main.hs
+++ src/main/Main.hs
@@ -207,7 +207,7 @@ buildCmd finalAction opts logLevel =
               display range <>
               "), but " <>
               versionString version <>
-              " is provided locally"
+              " is provided by your snapshot"
             StackageDepVerMismatch name ver range ->
               ("The package '" <> packageNameString name <>
                "' in this Stackage snapshot is " <> versionString ver <>
