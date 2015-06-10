e0c5b8f48a92a8041a039639207013647c5fa82a
Chris Done <chrisdone@gmail.com>
2015-06-10 12:24:55 +0200

    Setup sticky in StackT

--- src/Stack/Types/Internal.hs
+++ src/Stack/Types/Internal.hs
@@ -2,15 +2,19 @@
 
 module Stack.Types.Internal where
 
-import Control.Monad.Logger (LogLevel)
-import Network.HTTP.Client.Conduit (Manager,HasHttpManager(..))
-import Stack.Types.Config
+import           Control.Concurrent.MVar
+import           Control.Monad.Logger (LogLevel)
+import           Data.ByteString (ByteString)
+
+import           Network.HTTP.Client.Conduit (Manager,HasHttpManager(..))
+import           Stack.Types.Config
 
 -- | Monadic environment.
 data Env config =
   Env {envConfig :: !config
       ,envLogLevel :: !LogLevel
-      ,envManager :: !Manager}
+      ,envManager :: !Manager
+      ,envSticky :: !(MVar Sticky)}
 
 instance HasStackRoot config => HasStackRoot (Env config) where
     getStackRoot = getStackRoot . envConfig
@@ -32,3 +36,15 @@ instance HasLogLevel (Env config) where
 
 instance HasLogLevel LogLevel where
   getLogLevel = id
+
+data Sticky = Sticky
+    { stickyCurrentLine :: !(Maybe ByteString)
+    , stickyMaxColumns :: !Int
+    , stickyLastWasSticky :: !Bool
+    }
+
+class HasSticky r where
+    getSticky :: r -> MVar Sticky
+
+instance HasSticky (Env config) where
+  getSticky = envSticky
