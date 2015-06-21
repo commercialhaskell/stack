c9ce512b80adb09fe798c47ac74b54a67ee66d99
Tristan Webb <tristan@fpcomplete.com>
2015-06-21 02:19:17 -0700

    ImportSystem module for running system commands in tests

---
+++ test/integration/lib/ImportSystem.hs
@@ -0,0 +1,9 @@
+module ImportSystem (module ImportSystem) where
+
+import Control.Exception as ImportSystem
+import System.Environment as ImportSystem
+import System.FilePath as ImportSystem
+import System.Directory as ImportSystem
+import System.IO as ImportSystem
+import System.Process as ImportSystem
+import System.Exit as ImportSystem
