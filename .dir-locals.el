55cb277d2b43da208f660689e18902d185af9ba1
Chris Done <chrisdone@gmail.com>
2015-06-28 11:22:47 +0200

    Update .dir-locals.el with haskell-cabal-mode

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,10 @@
-((haskell-mode . ((haskell-indent-spaces . 4)
-                  (hindent-style . "johan-tibell")
-                  (haskell-process-type . ghci)
-                  (haskell-process-path-ghci . "stack")
-                  (haskell-process-args-ghci . ("ghci")))))
+((haskell-mode
+  . ((haskell-indent-spaces . 4)
+     (hindent-style . "johan-tibell")
+     (haskell-process-type . ghci)
+     (haskell-process-path-ghci . "stack")
+     (haskell-process-args-ghci . ("ghci"))))
+ (haskell-cabal-mode
+  . ((haskell-process-type . ghci)
+     (haskell-process-path-ghci . "stack")
+     (haskell-process-args-ghci . ("ghci")))))
