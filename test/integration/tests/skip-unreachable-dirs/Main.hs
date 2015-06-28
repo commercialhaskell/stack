6ada82b64382a89f672a4a8731ce22effba16af7
Nikita Uvarov <uv.nikita@gmail.com>
2015-06-28 19:51:39 +0300

    Skip unreachable directories during init
    Catch permission denied exception in findFiles function and ignore such
    directories.

---
+++ test/integration/tests/skip-unreachable-dirs/Main.hs
@@ -0,0 +1,8 @@
+import StackTest
+import System.Directory (setPermissions, emptyPermissions, createDirectory)
+
+main :: IO ()
+main = do
+    createDirectory "unreachabledir"
+    setPermissions  "unreachabledir" $ emptyPermissions
+    stack ["init"]
