3a48d5f002483ea3f854ee47aaaf4c96efd505df
Michael Snoyman <michael@fpcomplete.com>
2015-08-11 07:45:13 +0300

    Disable no-longer-needed test

--- src/test/Stack/Build/ExecuteSpec.hs
+++ src/test/Stack/Build/ExecuteSpec.hs
@@ -8,17 +8,4 @@ main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec = describe "compareTestComponents" $ do
-    let test comps names expected = it (show (comps, names)) $
-            compareTestsComponents
-                (T.words $ T.pack comps)
-                (T.words $ T.pack names)
-                `shouldBe`
-                (T.words $ T.pack expected)
-
-    test "" "" ""
-    test "" "foo" "foo"
-    test "foo" "bar" ""
-    test "foo" "foo bar" "foo"
-    test "test:foo" "foo bar" "foo"
-    test "test:foo exe:bar" "foo bar" "foo"
+spec = return ()
