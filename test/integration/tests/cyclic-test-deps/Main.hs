54fca6bc0ca9a4eddb35dd74a782ef5506685292
Michael Snoyman <michael@snoyman.com>
2015-08-13 07:52:05 +0300

    Switch integration tests to use GHC 7.10

--- test/integration/tests/cyclic-test-deps/Main.hs
+++ test/integration/tests/cyclic-test-deps/Main.hs
@@ -3,5 +3,5 @@ import StackTest
 main :: IO ()
 main = do
     stack ["unpack", "text-1.2.1.1"]
-    stack ["init", "--resolver", "lts-2.9"]
+    stack ["init", "--resolver", "lts-3.0"]
     stack ["test", "--dry-run"]
