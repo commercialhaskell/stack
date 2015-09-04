d61516e900fde97235e6c7b8de85604aa15d864a
Emanuel Borsboom <manny@fpcomplete.com>
2015-09-03 23:01:03 -0700

    release.hs: build Stack's haddocks as part of 'check'

--- scripts/release/release.hs
+++ scripts/release/release.hs
@@ -156,7 +156,7 @@ rules global@Global{..} args = do
         liftIO $ renameFile instExeFile tmpExeFile
         actionFinally
             (do opt <- addPath [installBinDir] []
-                () <- cmd opt stackProgName (stackArgs global) "build --pedantic"
+                () <- cmd opt stackProgName (stackArgs global) "build --pedantic --haddock --no-haddock-deps"
                 () <- cmd opt stackProgName (stackArgs global) "clean"
                 () <- cmd opt stackProgName (stackArgs global) "build"
                 () <- cmd opt stackProgName (stackArgs global) "test --flag stack:integration-tests"
