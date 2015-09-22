6736b461b8809325d6ba55ef7081d1f177f3ac4a
Emanuel Borsboom <manny@fpcomplete.com>
2015-09-22 09:36:24 -0700

    Fix 32-unlisted-module test on Windows (handle different newlines)

--- test/integration/tests/32-unlisted-module/files/src/Main.hs
+++ test/integration/tests/32-unlisted-module/files/src/Main.hs
@@ -2,14 +2,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import qualified Data.ByteString.Char8 as C8
 import Data.FileEmbed
+import Data.List
 import Unlisted
 
 main :: IO ()
 main = do
     putStrLn ("main " ++ show foo ++ " " ++ show embedded)
-    if embedded == "FAIL\n"
+    if "FAIL" `C8.isPrefixOf` embedded
         then error "embedded contains FAIL"
         else return ()
 
-embedded  = $(embedFile "embed.txt")
+embedded = $(embedFile "embed.txt")
