7b74b49f094a7edf1835c6d0e7a10a86d9713c78
Chris Done <chrisdone@gmail.com>
2015-09-22 22:38:52 +0200

    Properly specify -i to stack-ide as --include

--- src/Stack/Ide.hs
+++ src/Stack/Ide.hs
@@ -11,39 +11,26 @@ module Stack.Ide
     (ide, getPackageOptsAndTargetFiles)
     where
 
-import           Control.Concurrent
 import           Control.Monad.Catch
 import           Control.Monad.IO.Class
 import           Control.Monad.Logger
 import           Control.Monad.Reader
 import           Control.Monad.Trans.Control (MonadBaseControl)
-import           Data.Aeson
-import qualified Data.ByteString as S
-import qualified Data.ByteString.Char8 as S8
-import           Data.ByteString.Lazy (ByteString)
-import qualified Data.ByteString.Lazy as L
 import           Data.List
 import           Data.Maybe
 import           Data.Monoid
 import qualified Data.Set as S
 import           Data.Text (Text)
-import qualified Data.Text as T
 import           Distribution.System
 import           Network.HTTP.Client.Conduit
 import           Path
 import           Path.IO
 import           Stack.Constants
-import           Stack.Exec (defaultEnvSettings)
 import           Stack.Ghci (GhciPkgInfo(..), ghciSetup)
 import           Stack.Package
 import           Stack.Types
 import           Stack.Types.Internal
-import           System.Directory (doesFileExist)
 import           System.Environment (lookupEnv)
-import           System.Exit
-import           System.IO
-import qualified System.Process as P
-import           System.Process.Read
 import           System.Process.Run
 
 -- | Launch a GHCi IDE for the given local project targets with the
@@ -57,7 +44,7 @@ ide
 ide targets useropts = do
     (_realTargets,_,pkgs) <- ghciSetup Nothing targets
     pwd <- getWorkingDir
-    (pkgopts,srcfiles) <-
+    (pkgopts,_srcfiles) <-
         liftM mconcat $ forM pkgs $ getPackageOptsAndTargetFiles pwd
     localdb <- packageDatabaseLocal
     depsdb <- packageDatabaseDeps
@@ -70,16 +57,25 @@ ide targets useropts = do
               intercalate ":" (map toFilePath bindirs) <>
               (maybe "" (':' :) mpath)]
         args =
-            ["--verbose"] <> ["--local-work-dir=" ++ toFilePath pwd] <>
+            ["--verbose"] <> ["--include=" <> includeDirs pkgopts] <>
+            ["--local-work-dir=" ++ toFilePath pwd] <>
             map ("--ghc-option=" ++) useropts <>
             paths <>
             pkgopts <>
             pkgdbs
     menv <- getMinimalEnvOverride
     Platform _ os <- asks getPlatform
-    when (os == OSX)
-         (callProcess (Just pwd) menv "stty" ["cbreak","-imaxbel"])
+    when
+        (os == OSX)
+        (callProcess (Just pwd) menv "stty" ["cbreak", "-imaxbel"])
     callProcess (Just pwd) menv "stack-ide" args
+  where
+    includeDirs pkgopts =
+        intercalate
+            ":"
+            (mapMaybe
+                 (stripPrefix "--ghc-option=-i")
+                 pkgopts)
 
 -- | Get options and target files for the given package info.
 getPackageOptsAndTargetFiles
