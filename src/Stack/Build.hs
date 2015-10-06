26ef057497441e3ad2794615a7cbc216a8eef92e
Michael Snoyman <michael@snoyman.com>
2015-10-06 18:52:14 +0300

    Fix another warning

--- src/Stack/Build.hs
+++ src/Stack/Build.hs
@@ -30,7 +30,6 @@ import qualified Data.Map as Map
 import           Data.Map.Strict (Map)
 import           Data.Set (Set)
 import qualified Data.Set as Set
-import qualified Data.Text as T
 import           Network.HTTP.Client.Conduit (HasHttpManager)
 import           Path
 import           Path.IO
@@ -52,6 +51,7 @@ import           System.FileLock (FileLock, unlockFile)
 #ifdef WINDOWS
 import System.Win32.Console (setConsoleCP, setConsoleOutputCP, getConsoleCP, getConsoleOutputCP)
 import qualified Control.Monad.Catch as Catch
+import qualified Data.Text as T
 #endif
 
 type M env m = (MonadIO m,MonadReader env m,HasHttpManager env,HasBuildConfig env,MonadLogger m,MonadBaseControl IO m,MonadCatch m,MonadMask m,HasLogLevel env,HasEnvConfig env,HasTerminal env)
