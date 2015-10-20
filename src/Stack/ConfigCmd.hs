c86302276b209866d0814afbab5089d10dfa8f43
Tristan Webb <tristan@fpcomplete.com>
2015-10-20 08:57:10 -0700

    Fix 7.8.4 compile and remove unnecessary

--- src/Stack/ConfigCmd.hs
+++ src/Stack/ConfigCmd.hs
@@ -15,8 +15,7 @@ import           Control.Monad.IO.Class
 import           Control.Monad.Logger
 import           Control.Monad.Reader (MonadReader, asks)
 import           Control.Monad.Trans.Control (MonadBaseControl)
-import qualified Data.ByteString.Builder         as B
-import qualified Data.ByteString.Lazy            as L
+import qualified Data.ByteString as S
 import qualified Data.HashMap.Strict as HMap
 import qualified Data.Yaml as Yaml
 import           Network.HTTP.Client.Conduit (HasHttpManager)
@@ -39,14 +38,14 @@ cfgCmdSet :: ( MonadIO m
              , MonadLogger m)
              => ConfigCmdSet -> m ()
 cfgCmdSet (ConfigCmdSetResolver newResolver) = do
-    stackYaml <- bcStackYaml <$> asks getBuildConfig
+    stackYaml <- fmap bcStackYaml (asks getBuildConfig)
     let stackYamlFp =
             toFilePath stackYaml
     -- We don't need to worry about checking for a valid yaml here
     (projectYamlConfig :: Yaml.Object) <-
         liftIO (Yaml.decodeFileEither stackYamlFp) >>=
         either throwM return
-    newResolverText <- resolverName <$> makeConcreteResolver newResolver
+    newResolverText <- fmap resolverName (makeConcreteResolver newResolver)
     -- We checking here that the snapshot actually exists
     snap <- parseSnapName newResolverText
     _ <- loadMiniBuildPlan snap
@@ -57,11 +56,9 @@ cfgCmdSet (ConfigCmdSetResolver newResolver) = do
                 (Yaml.String newResolverText)
                 projectYamlConfig
     liftIO
-        (L.writeFile
+        (S.writeFile
              stackYamlFp
-             (B.toLazyByteString
-                  (B.byteString
-                       (Yaml.encode projectYamlConfig'))))
+             (Yaml.encode projectYamlConfig'))
     return ()
 
 cfgCmdName :: String
