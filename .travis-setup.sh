a780bef62ef7d9fe29fa17f7514a164ff75fe3fa
Oleg Grenrus <oleg.grenrus@iki.fi>
2015-11-23 10:15:38 +0200

    Build on travis with cabal too

--- .travis-setup.sh
+++ .travis-setup.sh
@@ -2,9 +2,21 @@
 
 set -eux
 
+case "$BUILD" in
+  stack)
+    mkdir -p ~/.local/bin;
+    if [ `uname` = "Darwin" ]; then
+      curl -L https://www.stackage.org/stack/osx-x86_64 | tar xz --strip-components=1 --include '*/stack' -C ~/.local/bin;
+    else
+      curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack';
+    fi
+    stack --no-terminal setup;;
+  cabal)
 mkdir -p $HOME/.cabal
 cat > $HOME/.cabal/config <<EOF
 remote-repo: hackage.haskell.org:http://hackage.fpcomplete.com/
 remote-repo-cache: $HOME/.cabal/packages
 jobs: \$ncpus
 EOF
+;;
+esac
