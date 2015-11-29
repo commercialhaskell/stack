ce3e93f1a7f9056de5fa394a0640ed5b9a724775
Emanuel Borsboom <emanuel.borsboom@gmail.com>
2015-11-29 11:39:29 -0800

    Travis: use pvp-bounds when building with cabal-install (#1450)

--- .travis-setup.sh
+++ .travis-setup.sh
@@ -2,15 +2,17 @@
 
 set -eux
 
+mkdir -p ~/.local/bin;
+if [ `uname` = "Darwin" ]; then
+  curl -L https://www.stackage.org/stack/osx-x86_64 | tar xz --strip-components=1 --include '*/stack' -C ~/.local/bin;
+else
+  curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack';
+fi;
+stack --no-terminal setup;
+
 case "$BUILD" in
   stack)
-    mkdir -p ~/.local/bin;
-    if [ `uname` = "Darwin" ]; then
-      curl -L https://www.stackage.org/stack/osx-x86_64 | tar xz --strip-components=1 --include '*/stack' -C ~/.local/bin;
-    else
-      curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack';
-    fi
-    stack --no-terminal setup;;
+    ;;
   cabal)
 mkdir -p $HOME/.cabal
 cat > $HOME/.cabal/config <<EOF
