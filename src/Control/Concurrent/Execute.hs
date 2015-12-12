3f019014651cb1f2dac6bb3ecd930c7a0fdd19eb
Michael Sloan <mgsloan@gmail.com>
2015-12-11 18:22:18 -0800

    Fix progress output #1302 #1497

--- src/Control/Concurrent/Execute.hs
+++ src/Control/Concurrent/Execute.hs
@@ -11,7 +11,7 @@ module Control.Concurrent.Execute
     ) where
 
 import           Control.Applicative
-import           Control.Concurrent.Async (Concurrently (..), async)
+import           Control.Concurrent.Async (Concurrently (..), async, wait)
 import           Control.Concurrent.STM
 import           Control.Exception
 import           Control.Monad            (join, unless)
@@ -75,10 +75,11 @@ runActions threads keepGoing concurrentFinal actions0 withProgress = do
                 then pure Nothing
                 else Just <$> atomically (newTMVar ()))
         <*> pure keepGoing
-    _ <- async $ withProgress $ esCompleted es
+    progressThread <- async $ withProgress $ esCompleted es
     if threads <= 1
         then runActions' es
         else runConcurrently $ sequenceA_ $ replicate threads $ Concurrently $ runActions' es
+    () <- wait progressThread
     readTVarIO $ esExceptions es
 
 runActions' :: ExecuteState -> IO ()
