97dfb26cef87986783a896589a9ab0e42f48bc97
Emanuel Borsboom <manny@fpcomplete.com>
2015-12-28 06:52:41 -0800

    Travis: work around curl certificate issue on OS X

--- .travis-setup.sh
+++ .travis-setup.sh
@@ -4,7 +4,7 @@ set -eux
 
 mkdir -p ~/.local/bin;
 if [ `uname` = "Darwin" ]; then
-  curl -L https://www.stackage.org/stack/osx-x86_64 | tar xz --strip-components=1 --include '*/stack' -C ~/.local/bin;
+  curl -kL https://www.stackage.org/stack/osx-x86_64 | tar xz --strip-components=1 --include '*/stack' -C ~/.local/bin;
 else
   curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack';
 fi;
