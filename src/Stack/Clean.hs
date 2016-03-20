7f96f83c0e81b43c071169b682ab4e5c5fb356d8
Simon Jakobi <simon.jakobi@gmail.com>
2016-03-20 03:46:26 +0100

    Refactor Stack.Clean.clean

--- src/Stack/Clean.hs
+++ src/Stack/Clean.hs
@@ -8,21 +8,21 @@ module Stack.Clean
     ) where
 
 import           Control.Exception (Exception)
-import           Control.Monad (when)
-import           Control.Monad.Catch (MonadCatch, throwM)
+import           Control.Monad.Catch (MonadCatch, MonadThrow, throwM)
 import           Control.Monad.IO.Class (MonadIO)
 import           Control.Monad.Logger (MonadLogger)
-import           Control.Monad.Reader (MonadReader, asks)
+import           Control.Monad.Reader (MonadReader)
 import           Data.Foldable (forM_)
 import           Data.List ((\\),intercalate)
 import qualified Data.Map.Strict as Map
 import           Data.Maybe (mapMaybe)
 import           Data.Typeable (Typeable)
+import           Path (Path, Abs, Dir)
 import           Path.IO (ignoringAbsence, removeDirRecur)
 import           Stack.Build.Source (getLocalPackageViews)
 import           Stack.Build.Target (LocalPackageView(..))
 import           Stack.Constants (distDirFromDir, workDirFromDir)
-import           Stack.Types (HasEnvConfig,PackageName, bcWorkDir, getBuildConfig)
+import           Stack.Types (HasEnvConfig, PackageName, configProjectWorkDir)
 
 -- | Reset the build, i.e. remove the @dist@ directory
 -- (for example @.stack-work\/dist\/x84_64-linux\/Cabal-1.22.4.0@)
@@ -33,34 +33,29 @@ clean
     :: (MonadCatch m, MonadIO m, MonadReader env m, HasEnvConfig env, MonadLogger m)
     => CleanOpts
     -> m ()
-clean (CleanTargets targets) =
-    cleanup targets False
-clean CleanFull =
-    cleanup [] True
+clean cleanOpts = do
+    dirs <- dirsToDelete cleanOpts
+    forM_ dirs (ignoringAbsence . removeDirRecur)
 
-cleanup
-    :: (MonadCatch m, MonadIO m, MonadReader env m, HasEnvConfig env, MonadLogger m)
-    => [PackageName] -> Bool
-    -> m()
-cleanup targets doFullClean = do
-    locals <- getLocalPackageViews
-    case targets \\ Map.keys locals of
-        [] -> do
-            let lpvs =
-                    if null targets
-                        then Map.elems locals -- default to cleaning all local packages
-                        else mapMaybe (`Map.lookup` locals) targets
-            forM_ lpvs $ \(LocalPackageView{lpvRoot = pkgDir},_) -> do
-                let delDir =
-                          if doFullClean
-                              then workDirFromDir pkgDir
-                              else distDirFromDir pkgDir
-                ignoringAbsence . removeDirRecur =<< delDir
-            when doFullClean $ do
-                bconfig <- asks getBuildConfig
-                bcwd <- bcWorkDir bconfig
-                ignoringAbsence (removeDirRecur bcwd)
-        pkgs -> throwM (NonLocalPackages pkgs)
+dirsToDelete
+    :: (MonadThrow m, MonadIO m, MonadReader env m, HasEnvConfig env, MonadLogger m)
+    => CleanOpts
+    -> m [Path Abs Dir]
+dirsToDelete cleanOpts = do
+    localPkgViews <- getLocalPackageViews
+    let localPkgNames = Map.keys localPkgViews
+        getPkgDir pkgName = fmap (lpvRoot . fst) (Map.lookup pkgName localPkgViews)
+    case cleanOpts of
+        CleanTargets targets -> do
+            pkgsToClean <-
+                case targets \\ localPkgNames of
+                    [] -> return (if null targets then localPkgNames else targets)
+                    xs -> throwM (NonLocalPackages xs)
+            mapM distDirFromDir (mapMaybe getPkgDir pkgsToClean)
+        CleanFull -> do
+            pkgWorkDirs <- mapM workDirFromDir (mapMaybe getPkgDir localPkgNames)
+            projectWorkDir <- configProjectWorkDir
+            return (projectWorkDir : pkgWorkDirs)
 
 -- | Options for cleaning a project.
 data CleanOpts
