dbefaf2ed5c0596b17d411fb6c155ae9193639e5
Tim Dysinger <tim@dysinger.net>
2016-03-25 17:48:00 -0700

    cleanup
    - fix|clean hlint warnings
    - format hindint john-tibell style
    - 4 spaces & no tabs

--- src/Stack/Image.hs
+++ src/Stack/Image.hs
@@ -1,11 +1,8 @@
-{-# LANGUAGE ConstraintKinds #-}
-{-# LANGUAGE DeriveDataTypeable #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE ConstraintKinds   #-}
+{-# LANGUAGE FlexibleContexts  #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE TemplateHaskell   #-}
 
 -- | This module builds Docker (OpenContainer) images.
 module Stack.Image
@@ -38,48 +35,63 @@ type Assemble e m = (HasConfig e, HasTerminal e, MonadBaseControl IO m, MonadIO 
 
 -- | Stages the executables & additional content in a staging
 -- directory under '.stack-work'
-stageContainerImageArtifacts :: Build e m => m ()
+stageContainerImageArtifacts
+    :: Build e m
+    => m ()
 stageContainerImageArtifacts = do
     config <- asks getConfig
     workingDir <- getCurrentDir
-    forM_ (zip [0..] $ imgDockers $ configImage config) $ \(idx, opts) -> do
-        imageDir <- imageStagingDir workingDir idx
-        ignoringAbsence (removeDirRecur imageDir)
-        ensureDir imageDir
-        stageExesInDir opts imageDir
-        syncAddContentToDir opts imageDir
+    forM_
+        (zip [0 ..] (imgDockers $ configImage config))
+        (\(idx,opts) ->
+              do imageDir <- imageStagingDir workingDir idx
+                 ignoringAbsence (removeDirRecur imageDir)
+                 ensureDir imageDir
+                 stageExesInDir opts imageDir
+                 syncAddContentToDir opts imageDir)
 
 -- | Builds a Docker (OpenContainer) image extending the `base` image
 -- specified in the project's stack.yaml.  Then new image will be
 -- extended with an ENTRYPOINT specified for each `entrypoint` listed
 -- in the config file.
-createContainerImageFromStage :: Assemble e m => m ()
+createContainerImageFromStage
+    :: Assemble e m
+    => m ()
 createContainerImageFromStage = do
     config <- asks getConfig
     workingDir <- getCurrentDir
-    forM_ (zip [0..] $ imgDockers $ configImage config) $ \(idx, opts) -> do
-        imageDir <- imageStagingDir workingDir idx
-        createDockerImage opts imageDir
-        extendDockerImageWithEntrypoint opts imageDir
+    forM_
+        (zip [0 ..] (imgDockers $ configImage config))
+        (\(idx,opts) ->
+              do imageDir <- imageStagingDir workingDir idx
+                 createDockerImage opts imageDir
+                 extendDockerImageWithEntrypoint opts imageDir)
 
 -- | Stage all the Package executables in the usr/local/bin
 -- subdirectory of a temp directory.
-stageExesInDir :: Build e m => ImageDockerOpts -> Path Abs Dir -> m ()
+stageExesInDir
+    :: Build e m
+    => ImageDockerOpts -> Path Abs Dir -> m ()
 stageExesInDir opts dir = do
-    srcBinPath <-
-        liftM (</> $(mkRelDir "bin")) installationRootLocal
-    let destBinPath = dir </>
-            $(mkRelDir "usr/local/bin")
+    srcBinPath <- fmap (</> $(mkRelDir "bin")) installationRootLocal
+    let destBinPath = dir </> $(mkRelDir "usr/local/bin")
     ensureDir destBinPath
     case imgDockerExecutables opts of
         Nothing -> copyDirRecur srcBinPath destBinPath
-        Just exes -> forM_ exes $ \exe -> do
-            exeRelFile <- parseRelFile exe
-            copyFile (srcBinPath </> exeRelFile) (destBinPath </> exeRelFile)
+        Just exes ->
+            forM_
+                exes
+                (\exe ->
+                      do exeRelFile <- parseRelFile exe
+                         copyFile
+                             (srcBinPath </> exeRelFile)
+                             (destBinPath </> exeRelFile))
 
 -- | Add any additional files into the temp directory, respecting the
 -- (Source, Destination) mapping.
-syncAddContentToDir :: Build e m => ImageDockerOpts -> Path Abs Dir -> m ()
+syncAddContentToDir
+    :: Build e m
+    => ImageDockerOpts -> Path Abs Dir -> m ()
 syncAddContentToDir opts dir = do
     bconfig <- asks getBuildConfig
     let imgAdd = imgDockerAdd opts
@@ -90,17 +102,18 @@ syncAddContentToDir opts dir = do
                  destPath <- parseAbsDir dest
                  let destFullPath = dir </> dropRoot destPath
                  ensureDir destFullPath
-                 copyDirRecur
-                     (bcRoot bconfig </> sourcePath)
-                     destFullPath)
+                 copyDirRecur (bcRoot bconfig </> sourcePath) destFullPath)
 
 -- | Derive an image name from the project directory.
-imageName :: Path Abs Dir -> String
+imageName
+    :: Path Abs Dir -> String
 imageName = map toLower . toFilePathNoTrailingSep . dirname
 
 -- | Create a general purpose docker image from the temporary
 -- directory of executables & static content.
-createDockerImage :: Assemble e m => ImageDockerOpts -> Path Abs Dir -> m ()
+createDockerImage
+    :: Assemble e m
+    => ImageDockerOpts -> Path Abs Dir -> m ()
 createDockerImage dockerConfig dir = do
     menv <- getMinimalEnvOverride
     case imgDockerBase dockerConfig of
@@ -108,25 +121,26 @@ createDockerImage dockerConfig dir = do
         Just base -> do
             liftIO
                 (writeFile
-                     (toFilePath
-                          (dir </>
-                           $(mkRelFile "Dockerfile")))
+                     (toFilePath (dir </> $(mkRelFile "Dockerfile")))
                      (unlines ["FROM " ++ base, "ADD ./ /"]))
-            let args = [ "build"
-                       , "-t"
-                       , fromMaybe
-                             (imageName (parent (parent (parent dir))))
-                             (imgDockerImageName dockerConfig)
-                       , toFilePathNoTrailingSep dir]
-            callProcess $ Cmd Nothing "docker" menv args
+            let args =
+                    [ "build"
+                    , "-t"
+                    , fromMaybe
+                          (imageName (parent . parent . parent $ dir))
+                          (imgDockerImageName dockerConfig)
+                    , toFilePathNoTrailingSep dir]
+            callProcess (Cmd Nothing "docker" menv args)
 
--- | Extend the general purpose docker image with entrypoints (if
--- specified).
-extendDockerImageWithEntrypoint :: Assemble e m => ImageDockerOpts -> Path Abs Dir -> m ()
+-- | Extend the general purpose docker image with entrypoints (if specified).
+extendDockerImageWithEntrypoint
+    :: Assemble e m
+    => ImageDockerOpts -> Path Abs Dir -> m ()
 extendDockerImageWithEntrypoint dockerConfig dir = do
     menv <- getMinimalEnvOverride
-    let dockerImageName = fromMaybe
-                (imageName (parent (parent (parent dir))))
+    let dockerImageName =
+            fromMaybe
+                (imageName (parent . parent . parent $ dir))
                 (imgDockerImageName dockerConfig)
     let imgEntrypoints = imgDockerEntrypoints dockerConfig
     case imgEntrypoints of
@@ -134,46 +148,51 @@ extendDockerImageWithEntrypoint dockerConfig dir = do
         Just eps ->
             forM_
                 eps
-                (\ep -> do
-                      liftIO
-                          (writeFile
-                               (toFilePath
-                                    (dir </>
-                                     $(mkRelFile "Dockerfile")))
-                               (unlines
-                                    [ "FROM " ++ dockerImageName
-                                    , "ENTRYPOINT [\"/usr/local/bin/" ++
-                                      ep ++ "\"]"
-                                    , "CMD []"]))
-                      callProcess $ Cmd
-                          Nothing
-                          "docker"
-                          menv
-                          [ "build"
-                          , "-t"
-                          , dockerImageName ++ "-" ++ ep
-                          , toFilePathNoTrailingSep dir])
+                (\ep ->
+                      do liftIO
+                             (writeFile
+                                  (toFilePath
+                                       (dir </> $(mkRelFile "Dockerfile")))
+                                  (unlines
+                                       [ "FROM " ++ dockerImageName
+                                       , "ENTRYPOINT [\"/usr/local/bin/" ++
+                                         ep ++ "\"]"
+                                       , "CMD []"]))
+                         callProcess
+                             (Cmd
+                                  Nothing
+                                  "docker"
+                                  menv
+                                  [ "build"
+                                  , "-t"
+                                  , dockerImageName ++ "-" ++ ep
+                                  , toFilePathNoTrailingSep dir]))
 
 -- | The command name for dealing with images.
-imgCmdName :: String
+imgCmdName
+    :: String
 imgCmdName = "image"
 
 -- | The command name for building a docker container.
-imgDockerCmdName :: String
+imgDockerCmdName
+    :: String
 imgDockerCmdName = "container"
 
 -- | Convert image opts monoid to image options.
-imgOptsFromMonoid :: ImageOptsMonoid -> ImageOpts
-imgOptsFromMonoid ImageOptsMonoid{..} = ImageOpts
+imgOptsFromMonoid
+    :: ImageOptsMonoid -> ImageOpts
+imgOptsFromMonoid ImageOptsMonoid{..} =
+    ImageOpts
     { imgDockers = imgMonoidDockers
     }
 
 -- | Stack image exceptions.
 data StackImageException =
     StackImageDockerBaseUnspecifiedException
-    deriving (Typeable)
+    deriving ((Typeable))
 
 instance Exception StackImageException
 
 instance Show StackImageException where
-    show StackImageDockerBaseUnspecifiedException = "You must specify a base docker image on which to place your haskell executables."
+    show StackImageDockerBaseUnspecifiedException =
+        "You must specify a base docker image on which to place your haskell executables."
