86ad0cf318158c3176a48a7904c078a7f0cceb52
Tim Dysinger <tim@dysinger.net>
2016-04-18 15:02:45 -0700

    Remove redundant options (moved to Main)

--- src/Stack/Sig.hs
+++ src/Stack/Sig.hs
@@ -1,7 +1,3 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
-{-# LANGUAGE TemplateHaskell   #-}
-
 {-|
 Module      : Stack.Sig
 Description : GPG Signatures for Stack
@@ -12,18 +8,7 @@ Stability   : experimental
 Portability : POSIX
 -}
 
-module Stack.Sig (module Sig, signOpts) where
+module Stack.Sig (module Sig) where
 
-import Options.Applicative
 import Stack.Sig.GPG as Sig
 import Stack.Sig.Sign as Sig
-
--- | Options for commands that sign packages
-signOpts :: Parser (Bool, String)
-signOpts =
-    (,) <$>
-    switch (long "no-signature" <> help "Do not sign & upload signatures") <*>
-    strOption
-        (long "sig-server" <> metavar "URL" <> showDefault <>
-         value "https://sig.commercialhaskell.org" <>
-         help "URL")
