031df8aea6b5dd88ce3f88069ad56484dcb22d9b
Simon Jakobi <simon.jakobi@gmail.com>
2016-05-16 06:48:43 +0200

    Silence redundant constraint warnings for Applicative via mapM_ and forM_

--- src/Stack/Dot.hs
+++ src/Stack/Dot.hs
@@ -230,14 +230,14 @@ printLocalNodes dotOpts locals = liftIO $ Text.putStrLn (Text.intercalate "\n" l
         lpNodes = map (applyStyle . nodeName) (F.toList locals)
 
 -- | Print nodes without dependencies
-printLeaves :: (Applicative m, MonadIO m)
+printLeaves :: MonadIO m
             => Map PackageName (Set PackageName,Maybe Version)
             -> m ()
-printLeaves = F.traverse_ printLeaf . Map.keysSet . Map.filter Set.null . fmap fst
+printLeaves = F.mapM_ printLeaf . Map.keysSet . Map.filter Set.null . fmap fst
 
 -- | @printDedges p ps@ prints an edge from p to every ps
-printEdges :: (Applicative m, MonadIO m) => PackageName -> Set PackageName -> m ()
-printEdges package deps = F.for_ deps (printEdge package)
+printEdges :: MonadIO m => PackageName -> Set PackageName -> m ()
+printEdges package deps = F.forM_ deps (printEdge package)
 
 -- | Print an edge between the two package names
 printEdge :: MonadIO m => PackageName -> PackageName -> m ()
