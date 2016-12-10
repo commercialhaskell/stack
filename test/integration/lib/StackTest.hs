0d1a3d210d111f72f78cd13e350d58c98da9c0af
Emanuel Borsboom <manny@fpcomplete.com>
2016-12-10 05:34:43 -0800

    Disable test that require --install-ghc on Alpine Linux
    Alpine Linux does yet support installing GHC.

--- test/integration/lib/StackTest.hs
+++ test/integration/lib/StackTest.hs
@@ -173,6 +173,9 @@ exeExt = if isWindows then ".exe" else ""
 -- | Is the OS Windows?
 isWindows = os == "mingw32"
 
+-- | Is the OS Alpine Linux?
+getIsAlpine = doesFileExist "/etc/alpine-release"
+
 -- | To avoid problems with GHC version mismatch when a new LTS major
 -- version is released, pass this argument to @stack@ when running in
 -- a global context.  The LTS major version here should match that of
