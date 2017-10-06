commercialhaskell/stack

commit 6047a31594367ed222c6f5cc9dc3ccfe9ea77efc

Implement global hints

src/Stack/BuildPlan.hs
@@ -417,15 +417,15 @@ instance Show BuildPlanCheck where
 -- the packages.
 checkSnapBuildPlan
     :: (StackM env m, HasConfig env, HasGHCVariant env)
-    => EnvOverride
-    -> Path Abs Dir -- ^ project root, used for checking out necessary files
+    => Path Abs Dir -- ^ project root, used for checking out necessary files
     -> [GenericPackageDescription]
     -> Maybe (Map PackageName (Map FlagName Bool))
     -> SnapshotDef
     -> m BuildPlanCheck
-checkSnapBuildPlan menv root gpds flags snapshotDef = do
+checkSnapBuildPlan root gpds flags snapshotDef = do
     platform <- view platformL
-    rs <- loadSnapshot menv root snapshotDef
+    menv <- getMinimalEnvOverride
+    rs <- loadSnapshot menv Nothing root snapshotDef
 
     let
         compiler = lsCompilerVersion rs
@@ -452,12 +452,11 @@ checkSnapBuildPlan menv root gpds flags snapshotDef = do
 -- best as possible with the given 'GenericPackageDescription's.
 selectBestSnapshot
     :: (StackM env m, HasConfig env, HasGHCVariant env)
-    => EnvOverride
-    -> Path Abs Dir -- ^ project root, used for checking out necessary files
+    => Path Abs Dir -- ^ project root, used for checking out necessary files
     -> [GenericPackageDescription]
     -> NonEmpty SnapshotDef
     -> m (SnapshotDef, BuildPlanCheck)
-selectBestSnapshot menv root gpds snaps = do
+selectBestSnapshot root gpds snaps = do
     $logInfo $ "Selecting the best among "
                <> T.pack (show (NonEmpty.length snaps))
                <> " snapshots...\n"
@@ -470,7 +469,7 @@ selectBestSnapshot menv root gpds snaps = do
                 _ -> fmap (betterSnap old) mnew
 
         getResult snap = do
-            result <- checkSnapBuildPlan menv root gpds Nothing snap
+            result <- checkSnapBuildPlan root gpds Nothing snap
             reportResult result snap
             return (snap, result)
 

src/Stack/Init.hs
@@ -42,6 +42,7 @@ import           Stack.BuildPlan
 import           Stack.Config                    (getSnapshots,
                                                   makeConcreteResolver)
 import           Stack.Constants
+import           Stack.Snapshot                  (loadResolver)
 import           Stack.Solver
 import           Stack.Types.Build
 import           Stack.Types.BuildPlan
@@ -84,8 +85,13 @@ initProject whichCmd currDir initOpts mresolver = do
     cabalfps <- liftM concat $ mapM find dirs'
     (bundle, dupPkgs)  <- cabalPackagesCheck cabalfps noPkgMsg Nothing
 
-    (r, flags, extraDeps, rbundle) <- getDefaultResolver whichCmd dest initOpts
-                                                         mresolver bundle
+    (sd, flags, extraDeps, rbundle) <- getDefaultResolver whichCmd dest initOpts
+                                                          mresolver bundle
+
+    -- FIXME shouldn't really need to recalculate this, perhaps modify
+    -- definition of LoadedResolver to keep the `Either Request
+    -- FilePath`?
+    resolver <- parseCustomLocation (Just (toFilePath (parent dest))) (void (sdResolver sd))
 
     let ignored = Map.difference bundle rbundle
         dupPkgMsg
@@ -125,7 +131,7 @@ initProject whichCmd currDir initOpts mresolver = do
                 (\(n, v) -> PLIndex $ PackageIdentifierRevision (PackageIdentifier n v) Nothing)
                 (Map.toList extraDeps)
             , projectFlags = removeSrcPkgDefaultFlags gpds flags
-            , projectResolver = r
+            , projectResolver = resolver
             , projectCompiler = Nothing
             , projectExtraPackageDBs = []
             }
@@ -143,7 +149,7 @@ initProject whichCmd currDir initOpts mresolver = do
         toPkg dir = PLFilePath $ makeRelDir dir
         indent t = T.unlines $ fmap ("    " <>) (T.lines t)
 
-    $logInfo $ "Initialising configuration using resolver: " <> resolverName r
+    $logInfo $ "Initialising configuration using resolver: " <> resolverName (sdResolver sd)
     $logInfo $ "Total number of user packages considered: "
                <> T.pack (show (Map.size bundle + length dupPkgs))
 
@@ -338,7 +344,7 @@ getDefaultResolver
     -> Maybe AbstractResolver
     -> Map PackageName (Path Abs File, C.GenericPackageDescription)
        -- ^ Src package name: cabal dir, cabal package description
-    -> m ( Resolver
+    -> m ( SnapshotDef
          , Map PackageName (Map FlagName Bool)
          , Map PackageName Version
          , Map PackageName (Path Abs File, C.GenericPackageDescription))
@@ -346,19 +352,21 @@ getDefaultResolver
        --   , Flags for src packages and extra deps
        --   , Extra dependencies
        --   , Src packages actually considered)
-getDefaultResolver whichCmd stackYaml initOpts mresolver bundle =
-    maybe selectSnapResolver makeConcreteResolver mresolver
-      >>= getWorkingResolverPlan whichCmd stackYaml initOpts bundle
+getDefaultResolver whichCmd stackYaml initOpts mresolver bundle = do
+    sd <- maybe selectSnapResolver (makeConcreteResolver (Just root) >=> loadResolver) mresolver
+    getWorkingResolverPlan whichCmd stackYaml initOpts bundle sd
     where
+        root = toFilePath $ parent stackYaml
         -- TODO support selecting best across regular and custom snapshots
         selectSnapResolver = do
             let gpds = Map.elems (fmap snd bundle)
             snaps <- fmap getRecommendedSnapshots getSnapshots'
-            (s, r) <- selectBestSnapshot gpds snaps
+            sds <- mapM (loadResolver . ResolverSnapshot) snaps
+            (s, r) <- selectBestSnapshot (parent stackYaml) gpds sds
             case r of
                 BuildPlanCheckFail {} | not (omitPackages initOpts)
                         -> throwM (NoMatchingSnapshot whichCmd snaps)
-                _ -> return $ ResolverSnapshot s
+                _ -> return s
 
 getWorkingResolverPlan
     :: (StackM env m, HasConfig env, HasGHCVariant env)
@@ -367,30 +375,30 @@ getWorkingResolverPlan
     -> InitOpts
     -> Map PackageName (Path Abs File, C.GenericPackageDescription)
        -- ^ Src package name: cabal dir, cabal package description
-    -> Resolver
-    -> m ( Resolver
+    -> SnapshotDef
+    -> m ( SnapshotDef
          , Map PackageName (Map FlagName Bool)
          , Map PackageName Version
          , Map PackageName (Path Abs File, C.GenericPackageDescription))
-       -- ^ ( Resolver
+       -- ^ ( SnapshotDef
        --   , Flags for src packages and extra deps
        --   , Extra dependencies
        --   , Src packages actually considered)
-getWorkingResolverPlan whichCmd stackYaml initOpts bundle resolver = do
-    $logInfo $ "Selected resolver: " <> resolverName resolver
+getWorkingResolverPlan whichCmd stackYaml initOpts bundle sd = do
+    $logInfo $ "Selected resolver: " <> resolverName (sdResolver sd)
     go bundle
     where
         go info = do
-            eres <- checkBundleResolver whichCmd stackYaml initOpts info resolver
+            eres <- checkBundleResolver whichCmd stackYaml initOpts info sd
             -- if some packages failed try again using the rest
             case eres of
-                Right (f, edeps)-> return (resolver, f, edeps, info)
+                Right (f, edeps)-> return (sd, f, edeps, info)
                 Left ignored
                     | Map.null available -> do
                         $logWarn "*** Could not find a working plan for any of \
                                  \the user packages.\nProceeding to create a \
                                  \config anyway."
-                        return (resolver, Map.empty, Map.empty, Map.empty)
+                        return (sd, Map.empty, Map.empty, Map.empty)
                     | otherwise -> do
                         when (Map.size available == Map.size info) $
                             error "Bug: No packages to ignore"
@@ -415,11 +423,11 @@ checkBundleResolver
     -> InitOpts
     -> Map PackageName (Path Abs File, C.GenericPackageDescription)
        -- ^ Src package name: cabal dir, cabal package description
-    -> Resolver
+    -> SnapshotDef
     -> m (Either [PackageName] ( Map PackageName (Map FlagName Bool)
                                , Map PackageName Version))
-checkBundleResolver whichCmd stackYaml initOpts bundle resolver = do
-    result <- checkResolverSpec gpds Nothing resolver
+checkBundleResolver whichCmd stackYaml initOpts bundle sd = do
+    result <- checkSnapBuildPlan (parent stackYaml) gpds Nothing sd
     case result of
         BuildPlanCheckOk f -> return $ Right (f, Map.empty)
         BuildPlanCheckPartial f e
@@ -430,7 +438,7 @@ checkBundleResolver whichCmd stackYaml initOpts bundle resolver = do
                 warnPartial result
                 $logWarn "*** Omitting packages with unsatisfied dependencies"
                 return $ Left $ failedUserPkgs e
-            | otherwise -> throwM $ ResolverPartial whichCmd resolver (show result)
+            | otherwise -> throwM $ ResolverPartial whichCmd (void resolver) (show result)
         BuildPlanCheckFail _ e _
             | omitPackages initOpts -> do
                 $logWarn $ "*** Resolver compiler mismatch: "
@@ -439,6 +447,7 @@ checkBundleResolver whichCmd stackYaml initOpts bundle resolver = do
                 return $ Left $ failedUserPkgs e
             | otherwise -> throwM $ ResolverMismatch whichCmd resolver (show result)
     where
+      resolver = sdResolver sd
       indent t  = T.unlines $ fmap ("    " <>) (T.lines t)
       warnPartial res = do
           $logWarn $ "*** Resolver " <> resolverName resolver
@@ -453,7 +462,7 @@ checkBundleResolver whichCmd stackYaml initOpts bundle resolver = do
               srcConstraints = mergeConstraints (gpdPackages gpds) flags
 
           eresult <- solveResolverSpec stackYaml cabalDirs
-                                       (resolver, srcConstraints, Map.empty)
+                                       (sd, srcConstraints, Map.empty)
           case eresult of
               Right (src, ext) ->
                   return $ Right (fmap snd (Map.union src ext), fmap fst ext)
@@ -470,7 +479,8 @@ checkBundleResolver whichCmd stackYaml initOpts bundle resolver = do
       -- set of packages.
       findOneIndependent packages flags = do
           platform <- view platformL
-          (compiler, _) <- getResolverConstraints stackYaml resolver
+          menv <- getMinimalEnvOverride
+          (compiler, _) <- getResolverConstraints menv Nothing stackYaml sd
           let getGpd pkg = snd (fromJust (Map.lookup pkg bundle))
               getFlags pkg = fromJust (Map.lookup pkg flags)
               deps pkg = gpdPackageDeps (getGpd pkg) compiler platform

src/Stack/Types/BuildPlan.hs
@@ -25,6 +25,7 @@ module Stack.Types.BuildPlan
     , ModuleInfo (..)
     , moduleInfoVC
     , setCompilerVersion
+    , sdWantedCompilerVersion
     ) where
 
 import           Control.Applicative
@@ -91,6 +92,13 @@ data SnapshotDef = SnapshotDef
     -- affect, for example, the import parser in the script command.
     , sdGhcOptions :: !(Map PackageName [Text])
     -- ^ GHC options per package
+    , sdGlobalHints :: !(Map PackageName (Maybe Version))
+    -- ^ Hints about which packages are available globally. When
+    -- actually building code, we trust the package database provided
+    -- by GHC itself, since it may be different based on platform or
+    -- GHC install. However, when we want to check the compatibility
+    -- of a snapshot with some codebase without installing GHC (e.g.,
+    -- during stack init), we would use this field.
     }
     deriving (Show, Eq)
 
@@ -209,7 +217,7 @@ instance Store LoadedSnapshot
 instance NFData LoadedSnapshot
 
 loadedSnapshotVC :: VersionConfig LoadedSnapshot
-loadedSnapshotVC = storeVersionConfig "ls-v1" "_PgwTtH6gYwg-A72iUR6KwpJYho="
+loadedSnapshotVC = storeVersionConfig "ls-v1" "DeqDAikx2iAWITRFSzcOaQNuNQo="
 
 -- | Information on a single package for the 'LoadedSnapshot' which
 -- can be installed.
@@ -296,3 +304,7 @@ instance Monoid ModuleInfo where
 
 moduleInfoVC :: VersionConfig ModuleInfo
 moduleInfoVC = storeVersionConfig "mi-v2" "8ImAfrwMVmqoSoEpt85pLvFeV3s="
+
+-- | Determined the desired compiler version for this 'SnapshotDef'.
+sdWantedCompilerVersion :: SnapshotDef -> CompilerVersion
+sdWantedCompilerVersion = either id sdWantedCompilerVersion . sdParent

src/Stack/Config.hs
@@ -186,15 +186,15 @@ getSnapshots = do
 -- | Turn an 'AbstractResolver' into a 'Resolver'.
 makeConcreteResolver
     :: (StackMiniM env m, HasConfig env)
-    => AbstractResolver
+    => Maybe FilePath -- ^ root of project for resolving custom relative paths
+    -> AbstractResolver
     -> m Resolver
-makeConcreteResolver (ARResolver r) = do
-  mapM (parseCustomLocation (error "FIXME makeConcreteResolver")) r
-makeConcreteResolver ar = do
+makeConcreteResolver root (ARResolver r) = parseCustomLocation root r
+makeConcreteResolver root ar = do
     snapshots <- getSnapshots
     r <-
         case ar of
-            ARResolver r -> assert False $ makeConcreteResolver $ ARResolver r
+            ARResolver r -> assert False $ makeConcreteResolver root $ ARResolver r
             ARGlobal -> do
                 config <- view configL
                 implicitGlobalDir <- getImplicitGlobalProjectDir config
@@ -586,7 +586,7 @@ loadBuildConfig mproject config mresolver mcompiler = do
         case mresolver of
             Nothing -> return $ projectResolver project'
             Just aresolver ->
-                runReaderT (makeConcreteResolver aresolver) miniConfig
+                runReaderT (makeConcreteResolver (Just (toFilePath (parent stackYamlFP))) aresolver) miniConfig
     let project = project'
             { projectResolver = resolver
             , projectCompiler = mcompiler <|> projectCompiler project'
@@ -619,7 +619,7 @@ loadBuildConfig mproject config mresolver mcompiler = do
     getEmptyProject = do
       r <- case mresolver of
             Just aresolver -> do
-                r' <- runReaderT (makeConcreteResolver aresolver) miniConfig
+                r' <- runReaderT (makeConcreteResolver Nothing aresolver) miniConfig
                 $logInfo ("Using resolver: " <> resolverName r' <> " specified on command line")
                 return r'
             Nothing -> do

src/Stack/Config/Docker.hs
@@ -4,6 +4,7 @@
 module Stack.Config.Docker where
 
 import           Control.Exception.Lifted
+import           Control.Monad (void)
 import           Control.Monad.Catch (MonadThrow)
 import           Data.List (find)
 import           Data.Maybe
@@ -38,7 +39,7 @@ dockerOptsFromMonoid mproject stackRoot maresolver DockerOptsMonoid{..} = do
                                 (ResolverNotSupportedException $
                                  show aresolver)
                         Nothing ->
-                            fmap ((fmap.fmap) snd projectResolver) mproject
+                            fmap (void . projectResolver) mproject
                 defaultTag =
                     case mresolver of
                         Nothing -> ""

src/Stack/ConfigCmd.hs
@@ -35,6 +35,7 @@ import           Stack.Snapshot (loadResolver)
 import           Stack.Types.Config
 import           Stack.Types.Resolver
 import           Stack.Types.StringError
+import           System.FilePath (takeDirectory)
 
 data ConfigCmdSet
     = ConfigCmdSetResolver AbstractResolver
@@ -75,7 +76,7 @@ cfgCmdSet go cmd = do
     -- We don't need to worry about checking for a valid yaml here
     (config :: Yaml.Object) <-
         liftIO (Yaml.decodeFileEither configFilePath) >>= either throwM return
-    newValue <- cfgCmdSetValue cmd
+    newValue <- cfgCmdSetValue (takeDirectory configFilePath) cmd
     let cmdKey = cfgCmdSetOptionName cmd
         config' = HMap.insert cmdKey newValue config
     if config' == config
@@ -88,19 +89,20 @@ cfgCmdSet go cmd = do
 
 cfgCmdSetValue
     :: (StackMiniM env m, HasConfig env, HasGHCVariant env)
-    => ConfigCmdSet -> m Yaml.Value
-cfgCmdSetValue (ConfigCmdSetResolver newResolver) = do
-    concreteResolver <- makeConcreteResolver newResolver
+    => FilePath -- ^ root directory of project
+    -> ConfigCmdSet -> m Yaml.Value
+cfgCmdSetValue root (ConfigCmdSetResolver newResolver) = do
+    concreteResolver <- makeConcreteResolver (Just root) newResolver
     case concreteResolver of
         -- Check that the snapshot actually exists
         ResolverSnapshot snapName -> void $ loadResolver $ ResolverSnapshot snapName
         ResolverCompiler _ -> return ()
         -- TODO: custom snapshot support?  Would need a way to specify on CLI
-        ResolverCustom _ _ -> errorString "'stack config set resolver' does not support custom resolvers"
+        ResolverCustom _ _ _ -> errorString "'stack config set resolver' does not support custom resolvers"
     return (Yaml.String (resolverName concreteResolver))
-cfgCmdSetValue (ConfigCmdSetSystemGhc _ bool) =
+cfgCmdSetValue _ (ConfigCmdSetSystemGhc _ bool) =
     return (Yaml.Bool bool)
-cfgCmdSetValue (ConfigCmdSetInstallGhc _ bool) =
+cfgCmdSetValue _ (ConfigCmdSetInstallGhc _ bool) =
     return (Yaml.Bool bool)
 
 cfgCmdSetOptionName :: ConfigCmdSet -> Text

src/Stack/Setup.hs
@@ -268,7 +268,11 @@ setupEnv mResolveMissingGHC = do
     let bcPath :: BuildConfig
         bcPath = set envOverrideL (const (return menv)) bc
 
-    ls <- runInnerStackT bcPath $ loadSnapshot menv (view projectRootL bc) (bcSnapshotDef bc)
+    ls <- runInnerStackT bcPath $ loadSnapshot
+      menv
+      (Just compilerVer)
+      (view projectRootL bc)
+      (bcSnapshotDef bc)
     let envConfig0 = EnvConfig
             { envConfigBuildConfig = bc
             , envConfigCabalVersion = cabalVer

src/Stack/Snapshot.hs
@@ -19,6 +19,7 @@ module Stack.Snapshot
   ) where
 
 import           Control.Applicative
+import           Control.Exception.Safe (impureThrow)
 import           Control.Monad (forM, unless, void)
 import           Control.Monad.Catch
 import           Control.Monad.IO.Class
@@ -143,6 +144,7 @@ loadResolver (ResolverSnapshot name) = do
                 (_, Just compiler) -> return compiler
                 _ -> fail "expected field \"ghc-version\" or \"compiler-version\" not present"
         let sdParent = Left compilerVersion'
+        sdGlobalHints <- si .: "core-packages"
 
         packages <- o .: "packages"
         (Endo mkLocs, sdFlags, sdHide) <- fmap mconcat $ mapM (uncurry goPkg) $ Map.toList packages
@@ -188,8 +190,9 @@ loadResolver (ResolverCompiler compiler) = return SnapshotDef
     , sdFlags = Map.empty
     , sdHide = Set.empty
     , sdGhcOptions = Map.empty
+    , sdGlobalHints = Map.empty
     }
-loadResolver (ResolverCustom name (loc, url)) = do
+loadResolver (ResolverCustom name url loc) = do
   $logDebug $ "Loading " <> url <> " build plan"
   case loc of
     Left req -> download' req >>= load
@@ -224,7 +227,7 @@ loadResolver (ResolverCustom name (loc, url)) = do
             case loc of
               Left _ -> Nothing
               Right fp' -> Just $ takeDirectory fp'
-      parentResolver' <- mapM (parseCustomLocation mdir) parentResolver
+      parentResolver' <- parseCustomLocation mdir parentResolver
 
       -- Calculate the hash of the current file, and then combine it
       -- with parent hashes if necessary below.
@@ -239,26 +242,27 @@ loadResolver (ResolverCustom name (loc, url)) = do
                 hash' = combineHash rawHash $
                   case sdResolver parent' of
                     ResolverSnapshot snapName -> snapNameToHash snapName
-                    ResolverCustom _ parentHash -> parentHash
+                    ResolverCustom _ _ parentHash -> parentHash
                     ResolverCompiler _ -> error "loadResolver: Receieved ResolverCompiler in impossible location"
             return (Right parent', hash')
       return sd0
         { sdParent = parent'
-        , sdResolver = ResolverCustom name hash'
+        , sdResolver = ResolverCustom name url hash'
         }
 
     -- | Note that the 'sdParent' and 'sdResolver' fields returned
     -- here are bogus, and need to be replaced with information only
     -- available after further processing.
     parseCustom :: Value
-                -> Parser (WithJSONWarnings (SnapshotDef, WithJSONWarnings (ResolverWith Text))) -- FIXME there should only be one WithJSONWarnings
+                -> Parser (WithJSONWarnings (SnapshotDef, WithJSONWarnings (ResolverWith ()))) -- FIXME there should only be one WithJSONWarnings
     parseCustom = withObjectWarnings "CustomSnapshot" $ \o -> (,)
         <$> (SnapshotDef (Left (error "loadResolver")) (ResolverSnapshot (LTS 0 0))
             <$> jsonSubWarningsT (o ..:? "packages" ..!= [])
             <*> o ..:? "drop-packages" ..!= Set.empty
             <*> o ..:? "flags" ..!= Map.empty
             <*> o ..:? "hide" ..!= Set.empty
-            <*> o ..:? "ghc-options" ..!= Map.empty)
+            <*> o ..:? "ghc-options" ..!= Map.empty
+            <*> o ..:? "global-hints" ..!= Map.empty)
         <*> o ..: "resolver"
 
     fromDigest :: Digest SHA256 -> SnapshotHash
@@ -277,32 +281,46 @@ loadResolver (ResolverCustom name (loc, url)) = do
 loadSnapshot
   :: forall env m.
      (StackMiniM env m, HasConfig env, HasGHCVariant env)
-  => EnvOverride
+  => EnvOverride -- ^ used for running Git/Hg, and if relevant, getting global package info
+  -> Maybe CompilerVersion -- ^ installed GHC we should query; if none provided, use the global hints
   -> Path Abs Dir -- ^ project root, used for checking out necessary files
   -> SnapshotDef
   -> m LoadedSnapshot
-loadSnapshot menv root sd = withCabalLoader $ \loader -> loadSnapshot' loader menv root sd
+loadSnapshot menv mcompiler root sd = withCabalLoader $ \loader -> loadSnapshot' loader menv mcompiler root sd
 
 -- | Fully load up a 'SnapshotDef' into a 'LoadedSnapshot'
 loadSnapshot'
   :: forall env m.
      (StackMiniM env m, HasConfig env, HasGHCVariant env)
-  => (PackageIdentifierRevision -> IO ByteString)
-  -> EnvOverride
+  => (PackageIdentifierRevision -> IO ByteString) -- ^ load a cabal file's contents from the index
+  -> EnvOverride -- ^ used for running Git/Hg, and if relevant, getting global package info
+  -> Maybe CompilerVersion -- ^ installed GHC we should query; if none provided, use the global hints
   -> Path Abs Dir -- ^ project root, used for checking out necessary files
   -> SnapshotDef
   -> m LoadedSnapshot
-loadSnapshot' loadFromIndex menv root =
+loadSnapshot' loadFromIndex menv mcompiler root =
     start
   where
     start (snapshotDefFixes -> sd) = do
-      path <- configLoadedSnapshotCache $ sdResolver sd -- FIXME confirm the path is by platform
+      path <- configLoadedSnapshotCache
+        (sdResolver sd)
+        (maybe GISSnapshotHints GISCompiler mcompiler)
       $(versionedDecodeOrLoad loadedSnapshotVC) path (inner sd)
 
     inner :: SnapshotDef -> m LoadedSnapshot
     inner sd = do
       LoadedSnapshot compilerVersion _ globals0 parentPackages0 <-
-        either loadCompiler start $ sdParent sd
+        case sdParent sd of
+          Left cv ->
+            case mcompiler of
+              Nothing -> return LoadedSnapshot
+                { lsCompilerVersion = cv
+                , lsResolver = ResolverCompiler cv
+                , lsGlobals = fromGlobalHints $ sdGlobalHints sd
+                , lsPackages = Map.empty
+                }
+              Just cv' -> loadCompiler cv'
+          Right sd' -> start sd'
 
       platform <- view platformL
 
@@ -371,6 +389,29 @@ loadSnapshot' loadFromIndex menv root =
           let res@(name', lpi) = calculate gpd platform compilerVersion loc flags hide options
           unless (name == name' && lpiVersion lpi0 == lpiVersion lpi) $ error "recalculate invariant violated"
           return res
+
+    fromGlobalHints :: Map PackageName (Maybe Version) -> Map PackageName (LoadedPackageInfo GhcPkgId)
+    fromGlobalHints =
+        Map.unions . map go . Map.toList
+      where
+        go (_, Nothing) = Map.empty
+        go (name, Just ver) = Map.singleton name LoadedPackageInfo
+          { lpiVersion = ver
+          -- For global hint purposes, we only care about the
+          -- version. All other fields are ignored when checking
+          -- project compatibility.
+          , lpiLocation = either impureThrow id
+                        $ parseGhcPkgId
+                        $ packageIdentifierText
+                        $ PackageIdentifier name ver
+          , lpiFlags = Map.empty
+          , lpiGhcOptions = []
+          , lpiPackageDeps = Map.empty
+          , lpiProvidedExes = Set.empty
+          , lpiNeededExes = Map.empty
+          , lpiExposedModules = Set.empty
+          , lpiHide = False
+          }
 
 -- | Ensure that all of the dependencies needed by this package
 -- are available in the given Map of packages.

commit 1914a948e14a70ceb9ce1c9dfebb471b4ae6a518

init: Load snapshots on demand during resolver selection
This fixes a regression from 6047a31594367ed222c6f5cc9dc3ccfe9ea77efc that
caused _all_ the recommended snapshots to be loaded upfront.

src/Stack/BuildPlan.hs
@@ -431,13 +431,13 @@ selectBestSnapshot
     :: (HasConfig env, HasGHCVariant env)
     => Path Abs Dir -- ^ project root, used for checking out necessary files
     -> [GenericPackageDescription]
-    -> NonEmpty SnapshotDef
+    -> NonEmpty SnapName
     -> RIO env (SnapshotDef, BuildPlanCheck)
 selectBestSnapshot root gpds snaps = do
     logInfo $ "Selecting the best among "
                <> T.pack (show (NonEmpty.length snaps))
                <> " snapshots...\n"
-    F.foldr1 go (NonEmpty.map getResult snaps)
+    F.foldr1 go (NonEmpty.map (getResult <=< loadResolver . ResolverSnapshot) snaps)
     where
         go mold mnew = do
             old@(_snap, bpc) <- mold

src/Stack/Init.hs
@@ -360,8 +360,7 @@ getDefaultResolver whichCmd stackYaml initOpts mresolver bundle = do
         selectSnapResolver = do
             let gpds = Map.elems (fmap snd bundle)
             snaps <- fmap getRecommendedSnapshots getSnapshots'
-            sds <- mapM (loadResolver . ResolverSnapshot) snaps
-            (s, r) <- selectBestSnapshot (parent stackYaml) gpds sds
+            (s, r) <- selectBestSnapshot (parent stackYaml) gpds snaps
             case r of
                 BuildPlanCheckFail {} | not (omitPackages initOpts)
                         -> throwM (NoMatchingSnapshot whichCmd snaps)