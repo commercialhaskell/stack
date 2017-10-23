0a790d46da33008e9316602cc8a730653c136f8e
Michael Sloan <mgsloan@gmail.com>
2017-10-22 17:05:11 -0700

    Due to revert of fix for #3315, also disable the test.
    See also #3353

--- test/integration/tests/3315-multi-ghc-options/Main.hs
+++ test/integration/tests/3315-multi-ghc-options/Main.hs
@@ -2,5 +2,8 @@ import StackTest
 
 main :: IO ()
 main = do
+  return ()
+  {- FIXME: Temporarily disabled.  See #3353 / #3315
   stack ["build", "--ghc-options=-DBAR -DBAZ"]
   stack ["setup", "--ghcjs-boot-options=-DBAR -DBAZ"]
+  -}
