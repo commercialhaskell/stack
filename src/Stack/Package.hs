6bc1269f0fdfc48d48e356174ad203238d65e493
Joshua Simmons <joshua.simmons@emptypath.com>
2017-12-09 15:53:46 -0700

    improve the warning when ignoring hpack file

--- src/Stack/Package.hs
+++ src/Stack/Package.hs
@@ -1354,8 +1354,11 @@ hpack pkgDir = do
                         ]
                     Hpack.ExistingCabalFileWasModifiedManually -> prettyWarnL
                         [ cabalFile
-                        , flow "was modified manually.  Ignoring package.yaml in favor of cabal file."
-                        , flow "If you want to use package.yaml instead of the cabal file, "
+                        , flow "was modified manually. Ignoring"
+                        , display hpackFile
+                        , flow "in favor of the cabal file. If you want to use the"
+                        , display . filename $ hpackFile
+                        , flow "file instead of the cabal file,"
                         , flow "then please delete the cabal file."
                         ]
             HpackCommand command -> do
