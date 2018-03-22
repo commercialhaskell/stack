59e22db740a8ee41a7062839ffdd635e9f3c01fb
Andrew Cowie <andrew@operationaldynamics.com>
2018-03-22 12:57:44 +1100

    Install zlib development package

--- etc/scripts/get-stack.sh
+++ etc/scripts/get-stack.sh
@@ -188,7 +188,7 @@ do_debian_install() {
 # and install the necessary dependencies explicitly.
 do_fedora_install() {
   install_dependencies() {
-    dnf_install_pkgs perl make automake gcc gmp-devel libffi zlib xz tar
+    dnf_install_pkgs perl make automake gcc gmp-devel libffi zlib-devel xz tar
   }
 
   if is_64_bit ; then
