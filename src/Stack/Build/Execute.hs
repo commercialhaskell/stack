8bfab462d1f555683d3371858c50eb535a5e3f99
Mihai Maruseac <mihai.maruseac@gmail.com>
2018-06-17 20:09:37 -0800

    Make sure we don't cache sublibraries when there is no library.

--- src/Stack/Build/Execute.hs
+++ src/Stack/Build/Execute.hs
@@ -1515,22 +1515,24 @@ singleBuild ac@ActionContext {..} ee@ExecuteEnv {..} task@Task {..} installedMap
                         ( bcoLocalDB eeBaseConfigOpts
                         , eeLocalDumpPkgs )
         let ident = PackageIdentifier (packageName package) (packageVersion package)
-        mpkgid <- case packageLibraries package of
+        -- only return the sublibs to cache them if we also cache the main lib (that is, if it exists)
+        (mpkgid, sublibsPkgIds) <- case packageLibraries package of
             HasLibraries _ -> do
+                sublibsPkgIds <- fmap (mapMaybe id) $
+                  forM (Set.toList $ packageInternalLibraries package) $ \sublib -> do
+                    -- z-haddock-library-z-attoparsec for internal lib attoparsec of haddock-library
+                    let sublibName = T.concat ["z-", packageNameText $ packageName package, "-z-", sublib]
+                    case parsePackageName sublibName of
+                      Nothing -> return Nothing -- invalid lib, ignored
+                      Just subLibName -> loadInstalledPkg wc [installedPkgDb] installedDumpPkgsTVar subLibName
+
                 mpkgid <- loadInstalledPkg wc [installedPkgDb] installedDumpPkgsTVar (packageName package)
                 case mpkgid of
                     Nothing -> throwM $ Couldn'tFindPkgId $ packageName package
-                    Just pkgid -> return $ Library ident pkgid Nothing
+                    Just pkgid -> return $ (Library ident pkgid Nothing, sublibsPkgIds)
             NoLibraries -> do
                 markExeInstalled (taskLocation task) taskProvides -- TODO unify somehow with writeFlagCache?
-                return $ Executable ident
-        sublibsPkgIds <- fmap (mapMaybe id) $
-          forM (Set.toList $ packageInternalLibraries package) $ \sublib -> do
-            -- z-haddock-library-z-attoparsec for internal lib attoparsec of haddock-library
-            let sublibName = T.concat ["z-", packageNameText $ packageName package, "-z-", sublib]
-            case parsePackageName sublibName of
-              Nothing -> return Nothing -- invalid lib, ignored
-              Just subLibName -> loadInstalledPkg wc [installedPkgDb] installedDumpPkgsTVar subLibName
+                return (Executable ident, []) -- don't return sublibs in this case
 
         case taskLocation task of
             Snap ->
