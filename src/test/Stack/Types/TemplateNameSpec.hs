0c5ad740c99d6263ba3c8313bab95c81e8f63384
John Mendonca <joaosinho@gmail.com>
2018-07-11 20:25:10 -0600

    TemplateNameSpec: describe template parsing on windows

--- src/test/Stack/Types/TemplateNameSpec.hs
+++ src/test/Stack/Types/TemplateNameSpec.hs
@@ -4,6 +4,7 @@ module Stack.Types.TemplateNameSpec where
 
 import Stack.Types.TemplateName
 import Path.Internal
+import System.Info (os)
 import Test.Hspec
 
 spec :: Spec
@@ -19,16 +20,32 @@ spec =
 
         pathOf "http://www.com/file"  `shouldBe` (UrlPath "http://www.com/file")
         pathOf "https://www.com/file" `shouldBe` (UrlPath "https://www.com/file")
-        
-        pathOf "//home/file"          `shouldBe` (AbsPath $ Path "/home/file.hsfiles")
-        pathOf "/home/file"           `shouldBe` (AbsPath $ Path "/home/file.hsfiles")
-        pathOf "/home/file.hsfiles"   `shouldBe` (AbsPath $ Path "/home/file.hsfiles")
-        
-        pathOf "with/slash"           `shouldBe` (RelPath $ Path "with/slash.hsfiles")
-        pathOf "with:colon"           `shouldBe` (RelPath $ Path "with:colon.hsfiles")
+
         pathOf "name"                 `shouldBe` (RelPath $ Path "name.hsfiles")
         pathOf "name.hsfile"          `shouldBe` (RelPath $ Path "name.hsfile.hsfiles")
         pathOf "name.hsfiles"         `shouldBe` (RelPath $ Path "name.hsfiles")
-        pathOf "c:\\home\\file"       `shouldBe` (RelPath $ Path "c:\\home\\file.hsfiles")
         pathOf ""                     `shouldBe` (RelPath $ Path ".hsfiles")
+
+        if os == "mingw32"
+        then do
+          pathOf "//home/file"          `shouldBe` (AbsPath $ Path "\\\\home\\file.hsfiles")
+          pathOf "/home/file"           `shouldBe` (RelPath $ Path "\\home\\file.hsfiles")
+          pathOf "/home/file.hsfiles"   `shouldBe` (RelPath $ Path "\\home\\file.hsfiles")
+
+          pathOf "c:\\home\\file"       `shouldBe` (AbsPath $ Path "C:\\home\\file.hsfiles")
+          pathOf "with/slash"           `shouldBe` (RelPath $ Path "with\\slash.hsfiles")
+
+          let colonAction =
+                do
+                  return $! pathOf "with:colon"
+          colonAction `shouldThrow` anyErrorCall
+
+        else do
+          pathOf "//home/file"          `shouldBe` (AbsPath $ Path "/home/file.hsfiles")
+          pathOf "/home/file"           `shouldBe` (AbsPath $ Path "/home/file.hsfiles")
+          pathOf "/home/file.hsfiles"   `shouldBe` (AbsPath $ Path "/home/file.hsfiles")
+
+          pathOf "c:\\home\\file"       `shouldBe` (RelPath $ Path "c:\\home\\file.hsfiles")
+          pathOf "with/slash"           `shouldBe` (RelPath $ Path "with/slash.hsfiles")
+          pathOf "with:colon"           `shouldBe` (RelPath $ Path "with:colon.hsfiles")
 
