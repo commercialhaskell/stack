444d094c72b13176cf97de22104f9942fd152f33
Michael Snoyman <michael@snoyman.com>
2018-08-10 02:21:28 +0300

    Fix a few more integration tests

--- test/integration/tests/3396-package-indices/Main.hs
+++ test/integration/tests/3396-package-indices/Main.hs
@@ -5,8 +5,11 @@ import           System.FilePath    ((</>))
 
 main :: IO ()
 main = do
+  putStrLn "With pantry, non-Hackage Security indices are no longer supported, skipping test"
+  {-
   home <- getEnv "HOME"
   setEnv "STACK_ROOT" (home </> ".stack") -- Needed for Windows
   createDirectoryIfMissing True (home </> ".stack" </> "indices" </> "CustomIndex")
   copy "CustomIndex/01-index.tar" (home </> ".stack"  </> "indices" </> "CustomIndex" </> "01-index.tar")
   stack ["build"]
+  -}
