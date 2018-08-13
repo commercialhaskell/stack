0378a0ee1b9809c55346652bcc47805b455103f3
Pawel Szulc <paul.szulc@gmail.com>
2018-08-13 14:11:29 +0200

    Add comments to integration tests

--- test/integration/tests/4045-no-run-benchmarks/Main.hs
+++ test/integration/tests/4045-no-run-benchmarks/Main.hs
@@ -4,10 +4,13 @@ import StackTest
 
 main :: IO ()
 main = do
-    stack [defaultResolverArg, "clean"]
+    -- stack [defaultResolverArg, "clean"]
     stack [defaultResolverArg, "build"]
+    -- given tests can be build, stack build --test
     stackOk ["test"]
+    -- given bench can NOT be build, stack build --bench
     stackFail ["bench"]
+    -- then running stack test --no-run-benchmarks" should fail
     stackFail ["test", "--no-run-benchmarks"]
 
 stackOk :: [String] -> IO ()
