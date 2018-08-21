9cad4a2aad76e5a904ed131892b494daee961514
Michael Snoyman <michael@snoyman.com>
2018-08-21 19:57:38 +0300

    Fix a hash

--- src/Stack/Types/BuildPlan.hs
+++ src/Stack/Types/BuildPlan.hs
@@ -144,7 +144,7 @@ configuration. Otherwise, we don't cache.
 -}
 
 loadedSnapshotVC :: VersionConfig LoadedSnapshot
-loadedSnapshotVC = storeVersionConfig "ls-v6" "ARoQclS4aNPX7uW8YMmM8-ZLrl0="
+loadedSnapshotVC = storeVersionConfig "ls-v6" "3Pdx94sRsLNSVm120unPRdAN5Is="
 
 -- | Information on a single package for the 'LoadedSnapshot' which
 -- can be installed.
