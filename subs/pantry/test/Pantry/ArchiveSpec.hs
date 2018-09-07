abc95802c3b44d9d1c9778043fa0af84a330b050
Brandon Chinn <brandon@leapyear.io>
2018-09-07 10:36:59 -0700

    Factor out common code in ArchiveSpec

--- subs/pantry/test/Pantry/ArchiveSpec.hs
+++ subs/pantry/test/Pantry/ArchiveSpec.hs
@@ -1,50 +1,70 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 module Pantry.ArchiveSpec (spec) where
 
 import Test.Hspec
+import Data.Maybe (fromJust)
 import RIO
+import RIO.Text as T
 import Pantry
 import Path.IO (resolveFile')
 
+data TestLocation
+  = TLFilePath String
+  | TLUrl Text
+
+data TestArchive = TestArchive
+  { testLocation :: !TestLocation
+  , testSubdir :: !Text
+  }
+
+getPackageLocationIdent' :: TestArchive -> IO PackageIdentifier
+getPackageLocationIdent' TestArchive{..} = do
+  testLocation' <- case testLocation of
+    TLFilePath relPath -> do
+      absPath <- resolveFile' relPath
+      return $ ALFilePath $ ResolvedPath
+        { resolvedRelative = RelFilePath $ fromString relPath
+        , resolvedAbsolute = absPath
+        }
+    TLUrl url -> return $ ALUrl url
+  let archive = Archive
+        { archiveLocation = testLocation'
+        , archiveHash = Nothing
+        , archiveSize = Nothing
+        , archiveSubdir = testSubdir
+        }
+  runPantryApp $ getPackageLocationIdent $ PLIArchive archive metadata
+  where
+    metadata = PackageMetadata
+      { pmName = Nothing
+      , pmVersion = Nothing
+      , pmTreeKey = Nothing
+      , pmCabal = Nothing
+      }
+
+parsePackageIdentifier' :: String -> PackageIdentifier
+parsePackageIdentifier' = fromJust . parsePackageIdentifier
+
+urlToStackCommit :: Text -> TestLocation
+urlToStackCommit commit = TLUrl $ T.concat
+  [ "https://github.com/commercialhaskell/stack/archive/"
+  , commit
+  , ".tar.gz"
+  ]
+
 spec :: Spec
 spec = do
-  it "cabal file from tarball" $ asIO $ runPantryApp $ do
-    let rel = "attic/package-0.1.2.3.tar.gz"
-    abs' <- resolveFile' rel
-    ident <- getPackageLocationIdent $ PLIArchive
-      Archive
-        { archiveLocation = ALFilePath ResolvedPath
-            { resolvedRelative = RelFilePath $ fromString rel
-            , resolvedAbsolute = abs'
-            }
-        , archiveHash = Nothing
-        , archiveSize = Nothing
-        , archiveSubdir = ""
-        }
-      PackageMetadata
-        { pmName = Nothing
-        , pmVersion = Nothing
-        , pmTreeKey = Nothing
-        , pmCabal = Nothing
-        }
-    case parsePackageIdentifier "package-0.1.2.3" of
-      Nothing -> error "should have parsed"
-      Just expected -> liftIO $ ident `shouldBe` expected
-  it "handles symlinks to parent dirs" $ do
-    ident <- runPantryApp $ getPackageLocationIdent $ PLIArchive
-      Archive
-        { archiveLocation = ALUrl "https://github.com/commercialhaskell/stack/archive/2b846ff4fda13a8cd095e7421ce76df0a08b10dc.tar.gz"
-        , archiveHash = Nothing
-        , archiveSize = Nothing
-        , archiveSubdir = "subs/pantry/"
-        }
-      PackageMetadata
-        { pmName = Nothing
-        , pmVersion = Nothing
-        , pmTreeKey = Nothing
-        , pmCabal = Nothing
-        }
-    case parsePackageIdentifier "pantry-0.1.0.0" of
-      Nothing -> error "should have parsed"
-      Just expected -> ident `shouldBe` expected
+  it "finds cabal file from tarball" $ do
+    ident <- getPackageLocationIdent' TestArchive
+      { testLocation = TLFilePath "attic/package-0.1.2.3.tar.gz"
+      , testSubdir = ""
+      }
+    ident `shouldBe` parsePackageIdentifier' "package-0.1.2.3"
+  it "finds cabal file from tarball with subdir 'subs/pantry/'" $ do
+    ident <- getPackageLocationIdent' TestArchive
+      { testLocation = urlToStackCommit "2b846ff4fda13a8cd095e7421ce76df0a08b10dc"
+      , testSubdir = "subs/pantry/"
+      }
+    ident `shouldBe` parsePackageIdentifier' "pantry-0.1.0.0"
