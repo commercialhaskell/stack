830f6e95ed5849677ab9b7941088a0b00df688b2
Mihai Maruseac <mihai.maruseac@gmail.com>
2018-09-14 19:55:18 -0700

    Update to use GPG key when signing

--- etc/scripts/with-vagrant.sh
+++ etc/scripts/with-vagrant.sh
@@ -4,7 +4,9 @@ set -xe
 export STACK_BUILD_DIR="$PWD"
 cd "$(dirname "$0")/../.."
 #TODO: set up gpg-agent forwarding for package signing (see http://superuser.com/questions/161973/how-can-i-forward-a-gpg-key-via-ssh-agent).
-gpg --export-secret-keys --armor `git config --get user.email` >"gpg-secret-key.asc~"
+# TODO: this should be from an envvar so we don't break existing pipelines
+GPG_USER_ID=`git config --get user.email`
+gpg --export-secret-keys --armor ${GPG_USER_ID} >"gpg-secret-key.asc~"
 cd "etc/vagrant/$1"
 
 # Double 'vagrant up' is a workaround for FreeBSD
@@ -13,5 +15,5 @@ vagrant up
 
 vagrant provision
 vagrant rsync
-vagrant ssh -c "set -xe; $3 export GITHUB_AUTH_TOKEN=$GITHUB_AUTH_TOKEN; gpg --import /vagrant/gpg-secret-key.asc~ || true; cd /vagrant-build; for x in CONTRIBUTING ChangeLog; do rm -f doc/\$x.md; ln -s ../\$x.md doc/\$x.md; done; stack /vagrant/etc/scripts/release.hs --no-test-haddocks $2"
+vagrant ssh -c "set -xe; $3 export GITHUB_AUTH_TOKEN=$GITHUB_AUTH_TOKEN; gpg --import /vagrant/gpg-secret-key.asc~ || true; cd /vagrant-build; for x in CONTRIBUTING ChangeLog; do rm -f doc/\$x.md; ln -s ../\$x.md doc/\$x.md; done; stack /vagrant/etc/scripts/release.hs --no-test-haddocks $2 --gpg-key ${GPG_USER_ID}"
 vagrant halt || vagrant halt -f
