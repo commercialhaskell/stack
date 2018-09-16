fa91a14b81137e5add21a74e998b4d474f545f22
Cthulhu.Den <cthulhu.den@gmail.com>
2018-09-16 11:14:07 +0300

    Apply hlint suggestion

--- test/integration/tests/4270-files-order/files/src/Lib.hs
+++ test/integration/tests/4270-files-order/files/src/Lib.hs
@@ -8,5 +8,6 @@ import System.IO.Unsafe
 foreign import ccall unsafe "a_dependent.h greeting"
   c_greeting :: CString
 
+{-# NOINLINE greeting #-}
 greeting :: String
 greeting = unsafePerformIO $ peekCString c_greeting
