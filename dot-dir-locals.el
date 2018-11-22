5c7b0b920c50db104d2b07d923476ec3fff6e770
Kirill Zaborsky <qrilka@gmail.com>
2018-11-22 16:15:17 +0300

    Remove obsolete hindent-style
    It was made obsolete in hindent 5

--- dot-dir-locals.el
+++ dot-dir-locals.el
@@ -1,6 +1,5 @@
 ((haskell-mode
   . ((haskell-indent-spaces . 4)
-     (hindent-style . "johan-tibell")
      (haskell-process-type . ghci)
      (haskell-process-path-ghci . "stack")
      (haskell-process-args-ghci . ("ghci"))))
