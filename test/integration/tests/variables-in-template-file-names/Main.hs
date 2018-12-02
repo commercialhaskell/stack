54f26da888e3a0d673d6f8d4d74a0ea3858d95a7
Emanuel Borsboom <manny@fpcomplete.com>
2018-12-02 09:50:32 -0800

    Fix integration test: variables-in-template-file-names

--- test/integration/tests/variables-in-template-file-names/Main.hs
+++ test/integration/tests/variables-in-template-file-names/Main.hs
@@ -4,7 +4,7 @@ import Control.Monad (unless)
 
 main :: IO ()
 main = do
-  removeDirectoryRecursiveIgnore "somename"
+  removeDirIgnore "somename"
   stack ["new", "somename", "./template.hsfiles"]
   exists <- doesFileExist "somename/somename.cabal"
   unless exists $ error "does not exist"
