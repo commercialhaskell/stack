a93694ce479e31726d8e6af5ba7a9761cdb98dd4
Sibi Prabakaran <sibi@psibi.in>
2018-12-21 23:20:26 +0530

    Cleanup

--- subs/pantry/src/Pantry/HPack.hs
+++ subs/pantry/src/Pantry/HPack.hs
@@ -2,7 +2,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-module Pantry.HPack (findOrGenerateCabalFile, hpack, hpackVersion, hpackToCabal) where
+module Pantry.HPack
+    (
+     hpack
+    , hpackVersion
+    ) where
 
 import RIO
 import RIO.Process
@@ -16,38 +20,6 @@ import qualified Hpack.Config as Hpack
 import Data.Char (isSpace, isDigit)
 import Path (Path, Abs, File, toFilePath, Dir, (</>), filename, parseAbsDir, parseRelFile, fromAbsFile)
 import Path.IO (doesFileExist, listDir)
-
--- | Get the filename for the cabal file in the given directory.
---
--- If no .cabal file is present, or more than one is present, an exception is
--- thrown via 'throwM'.
---
--- If the directory contains a file named package.yaml, hpack is used to
--- generate a .cabal file from it.
-findOrGenerateCabalFile
-    :: forall env. (HasPantryConfig env, HasLogFunc env, HasProcessContext env)
-    => Path Abs Dir -- ^ package directory
-    -> RIO env (PackageName, Path Abs File)
-findOrGenerateCabalFile pkgDir = do
-    hpack pkgDir
-    files <- filter (flip hasExtension "cabal" . toFilePath) . snd
-         <$> listDir pkgDir
-    -- If there are multiple files, ignore files that start with
-    -- ".". On unixlike environments these are hidden, and this
-    -- character is not valid in package names. The main goal is
-    -- to ignore emacs lock files - see
-    -- https://github.com/commercialhaskell/stack/issues/1897.
-    let isHidden ('.':_) = True
-        isHidden _ = False
-    case filter (not . isHidden . toFilePath . filename) files of
-        [] -> throwIO $ NoCabalFileFound pkgDir
-        [x] -> maybe
-          (throwIO $ InvalidCabalFilePath x)
-          (\pn -> pure $ (pn, x)) $
-            List.stripSuffix ".cabal" (toFilePath (filename x)) >>=
-            parsePackageName
-        _:_ -> throwIO $ MultipleCabalFilesFound pkgDir files
-      where hasExtension fp x = FilePath.takeExtension fp == "." ++ x
 
 
 hpackVersion
@@ -100,14 +72,3 @@ hpack pkgDir = do
             HpackCommand command ->
                 withWorkingDir (toFilePath pkgDir) $
                 proc command [] runProcess_
-
-
-hpackToCabal :: forall env. (HasPantryConfig env, HasLogFunc env, HasProcessContext env)
-           => ByteString -- Hpack's content
-           -> RIO env (PackageName, ByteString)
-hpackToCabal hpackBs = withSystemTempDirectory "hpack-pkg-dir" $ \tmpdir -> withWorkingDir tmpdir $ do
-               B.writeFile (tmpdir FilePath.</> Hpack.packageConfig) hpackBs
-               tdir <- parseAbsDir tmpdir
-               (packageName, cfile) <- findOrGenerateCabalFile tdir
-               bs <- B.readFile (fromAbsFile cfile)
-               return (packageName, bs)
