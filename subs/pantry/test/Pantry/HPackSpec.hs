0faabfd0f95fa9262e732f35e19690ce33047272
Sibi Prabakaran <sibi@psibi.in>
2018-12-28 20:23:21 +0530

    Update test according to new function

--- subs/pantry/test/Pantry/HPackSpec.hs
+++ subs/pantry/test/Pantry/HPackSpec.hs
@@ -1,12 +1,19 @@
 module Pantry.HPackSpec (spec) where
 
 import Test.Hspec
+import RIO
 import Pantry
 import Pantry.HPack
 import Path
 
 isVersion :: Version -> Bool
 isVersion _ = True
+
+customHpack :: (HasLogFunc env, HasPantryConfig env) => FilePath -> RIO env a -> RIO env a
+customHpack fp f = do
+  env <- ask
+  let a = set (pantryConfigL . hpackExecutableL) (HpackCommand fp) env
+  local (const a) f
 
 spec :: Spec
 spec = describe "Parse HPack version" $ do
@@ -15,6 +22,5 @@ spec = describe "Parse HPack version" $ do
     version `shouldSatisfy` isVersion
 
   -- it "External hpack version" $ do
-  --   fp <- parseAbsFile "/home/sibi/.local/bin/hpack"
-  --   version <- runPantryAppWithCustomHpack fp hpackVersion
+  --   version <- runPantryApp $ customHpack "/home/sibi/.local/bin/hpack" hpackVersion
   --   version `shouldSatisfy` isVersion
