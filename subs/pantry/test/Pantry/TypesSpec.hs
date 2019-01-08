889059cf5839bc396d0170335a8f4b044d920b36
Sibi Prabakaran <sibi@psibi.in>
2019-01-08 17:37:56 +0530

    Add corresponding test for fromJSON instance

--- subs/pantry/test/Pantry/TypesSpec.hs
+++ subs/pantry/test/Pantry/TypesSpec.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes#-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE FlexibleContexts #-}
 module Pantry.TypesSpec (spec) where
@@ -17,6 +19,7 @@ import qualified RIO.Text as T
 import qualified Data.Yaml as Yaml
 import Data.Aeson.Extended (WithJSONWarnings (..))
 import qualified Data.ByteString.Char8 as S8
+import Data.String.Quote
 
 hh :: HasCallStack => String -> Property -> Spec
 hh name p = it name $ do
@@ -28,6 +31,21 @@ genBlobKey = BlobKey <$> genSha256 <*> (FileSize <$> (Gen.word (Range.linear 1 1
 
 genSha256 :: Gen SHA256
 genSha256 = SHA256.hashBytes <$> Gen.bytes (Range.linear 1 500)
+
+samplePLIRepo :: ByteString
+samplePLIRepo = [s|
+subdir: wai
+cabal-file:
+  size: 1765
+  sha256: eea52c4967d8609c2f79213d6dffe6d6601034f1471776208404781de7051410
+name: wai
+version: 3.2.1.2
+git: https://github.com/yesodweb/wai.git
+pantry-tree:
+  size: 714
+  sha256: ecfd0b4b75f435a3f362394807b35e5ef0647b1a25005d44a3632c49db4833d2
+commit: d11d63f1a6a92db8c637a8d33e7953ce6194a3e0
+|]
 
 spec :: Spec
 spec = do
@@ -94,3 +112,16 @@ spec = do
         "name: 'test'\n" ++
         "compiler: ghc-8.0.1\n"
       rslParent `shouldBe` RSLCompiler (WCGhc (mkVersion [8, 0, 1]))
+
+    it "FromJSON instance for Repo" $ do
+      repValue <- case Yaml.decodeThrow samplePLIRepo of
+        Just (WithJSONWarnings x _) -> pure x
+        Nothing -> fail "Can't parse Repo"
+      let repoValue = Repo {
+                    repoSubdir = "wai",
+                    repoType = RepoGit,
+                    repoCommit = "d11d63f1a6a92db8c637a8d33e7953ce6194a3e0",
+                    repoUrl = "https://github.com/yesodweb/wai.git"
+                  }
+
+      repValue `shouldBe` repoValue
