a87c9bdfac8f589d2453fd4cda6a082b0b2d332d
Michael Snoyman <michael@snoyman.com>
2019-01-18 10:24:16 +0200

    Fix some hlint warnings

--- src/test/Stack/Types/TemplateNameSpec.hs
+++ src/test/Stack/Types/TemplateNameSpec.hs
@@ -14,26 +14,26 @@ spec =
       let pathOf s = either error templatePath (parseTemplateNameFromString s)
 
       it "parses out the TemplatePath" $ do
-        pathOf "github:user/name"     `shouldBe` (RepoPath $ RepoTemplatePath Github    "user" "name.hsfiles")
-        pathOf "bitbucket:user/name"  `shouldBe` (RepoPath $ RepoTemplatePath Bitbucket "user" "name.hsfiles")
-        pathOf "gitlab:user/name"     `shouldBe` (RepoPath $ RepoTemplatePath Gitlab    "user" "name.hsfiles")
+        pathOf "github:user/name"     `shouldBe` RepoPath (RepoTemplatePath Github    "user" "name.hsfiles")
+        pathOf "bitbucket:user/name"  `shouldBe` RepoPath (RepoTemplatePath Bitbucket "user" "name.hsfiles")
+        pathOf "gitlab:user/name"     `shouldBe` RepoPath (RepoTemplatePath Gitlab    "user" "name.hsfiles")
 
         pathOf "http://www.com/file"  `shouldBe` UrlPath "http://www.com/file"
         pathOf "https://www.com/file" `shouldBe` UrlPath "https://www.com/file"
 
-        pathOf "name"                 `shouldBe` (RelPath "name.hsfiles"        $ Path "name.hsfiles")
-        pathOf "name.hsfile"          `shouldBe` (RelPath "name.hsfile.hsfiles" $ Path "name.hsfile.hsfiles")
-        pathOf "name.hsfiles"         `shouldBe` (RelPath "name.hsfiles"        $ Path "name.hsfiles")
-        pathOf ""                     `shouldBe` (RelPath ".hsfiles"            $ Path ".hsfiles")
+        pathOf "name"                 `shouldBe` RelPath "name.hsfiles"        (Path "name.hsfiles")
+        pathOf "name.hsfile"          `shouldBe` RelPath "name.hsfile.hsfiles" (Path "name.hsfile.hsfiles")
+        pathOf "name.hsfiles"         `shouldBe` RelPath "name.hsfiles"        (Path "name.hsfiles")
+        pathOf ""                     `shouldBe` RelPath ".hsfiles"            (Path ".hsfiles")
 
         if os == "mingw32"
         then do
-          pathOf "//home/file"          `shouldBe` (AbsPath $ Path "\\\\home\\file.hsfiles")
-          pathOf "/home/file"           `shouldBe` (RelPath "/home/file.hsfiles" $ Path "\\home\\file.hsfiles")
-          pathOf "/home/file.hsfiles"   `shouldBe` (RelPath "/home/file.hsfiles" $ Path "\\home\\file.hsfiles")
+          pathOf "//home/file"          `shouldBe` AbsPath (Path "\\\\home\\file.hsfiles")
+          pathOf "/home/file"           `shouldBe` RelPath "/home/file.hsfiles" (Path "\\home\\file.hsfiles")
+          pathOf "/home/file.hsfiles"   `shouldBe` RelPath "/home/file.hsfiles" (Path "\\home\\file.hsfiles")
 
-          pathOf "c:\\home\\file"       `shouldBe` (AbsPath                      $ Path "C:\\home\\file.hsfiles")
-          pathOf "with/slash"           `shouldBe` (RelPath "with/slash.hsfiles" $ Path "with\\slash.hsfiles")
+          pathOf "c:\\home\\file"       `shouldBe` AbsPath                      (Path "C:\\home\\file.hsfiles")
+          pathOf "with/slash"           `shouldBe` RelPath "with/slash.hsfiles" (Path "with\\slash.hsfiles")
 
           let colonAction =
                 do
@@ -41,11 +41,11 @@ spec =
           colonAction `shouldThrow` anyErrorCall
 
         else do
-          pathOf "//home/file"          `shouldBe` (AbsPath $ Path "/home/file.hsfiles")
-          pathOf "/home/file"           `shouldBe` (AbsPath $ Path "/home/file.hsfiles")
-          pathOf "/home/file.hsfiles"   `shouldBe` (AbsPath $ Path "/home/file.hsfiles")
+          pathOf "//home/file"          `shouldBe` AbsPath (Path "/home/file.hsfiles")
+          pathOf "/home/file"           `shouldBe` AbsPath (Path "/home/file.hsfiles")
+          pathOf "/home/file.hsfiles"   `shouldBe` AbsPath (Path "/home/file.hsfiles")
 
-          pathOf "c:\\home\\file"       `shouldBe` (RelPath "c:\\home\\file.hsfiles" $ Path "c:\\home\\file.hsfiles")
-          pathOf "with/slash"           `shouldBe` (RelPath "with/slash.hsfiles"     $ Path "with/slash.hsfiles")
-          pathOf "with:colon"           `shouldBe` (RelPath "with:colon.hsfiles"     $ Path "with:colon.hsfiles")
+          pathOf "c:\\home\\file"       `shouldBe` RelPath "c:\\home\\file.hsfiles" (Path "c:\\home\\file.hsfiles")
+          pathOf "with/slash"           `shouldBe` RelPath "with/slash.hsfiles"     (Path "with/slash.hsfiles")
+          pathOf "with:colon"           `shouldBe` RelPath "with:colon.hsfiles"     (Path "with:colon.hsfiles")
 
