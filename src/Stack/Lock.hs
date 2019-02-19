ed4e800f851ea9b91a867c9167e3f69c953d747e
Sibi Prabakaran <sibi@psibi.in>
2019-02-19 14:45:45 +0530

    Add more utility functions

--- src/Stack/Lock.hs
+++ src/Stack/Lock.hs
@@ -6,20 +6,24 @@ module Stack.Lock where
 
 import qualified Data.Yaml as Yaml
 import Data.Yaml (object)
-import Path (addFileExtension, fromAbsFile)
+import Pantry (resolveSnapshotFile)
+import Path (addFileExtension, fromAbsFile, parent)
 import Path.IO (doesFileExist, getModificationTime)
 import qualified RIO.ByteString as B
 import Stack.Prelude
 import Stack.Types.Config
 
-data LockException =
-    LockNoProject
+data LockException
+    = LockNoProject
+    | LockCannotGenerate SnapshotLocation
     deriving (Typeable)
 
 instance Exception LockException
 
 instance Show LockException where
     show LockNoProject = "No project found for locking."
+    show (LockCannotGenerate e) =
+        "Lock file cannot be generated for snapshot: " <> (show e)
 
 generateLockFile :: Path Abs File -> RIO Config ()
 generateLockFile stackFile = do
@@ -53,12 +57,60 @@ generateLockFile stackFile = do
                 ]
     B.writeFile (fromAbsFile lockFile) (Yaml.encode depsObject)
 
-hasLockFile :: HasEnvConfig env => BuildConfig -> RIO env Bool
-hasLockFile bconfig = do
-    let stackFile = bcStackYaml bconfig
-    lockFile <- liftIO $ addFileExtension "lock" stackFile
-    liftIO $ doesFileExist lockFile
+loadSnapshotFile :: Path Abs File -> Path Abs Dir -> IO [RawPackageLocation]
+loadSnapshotFile path rootDir = do
+    val <- Yaml.decodeFileThrow (toFilePath path)
+    case Yaml.parseEither (resolveSnapshotFile rootDir) val of
+        Left str -> fail $ "Cannot parse snapshot file: Got error " <> str
+        Right rplio -> rplio
 
+loadSnapshotLockFile ::
+       Path Abs File -- ^ Snapshot lock file
+    -> IO [(RawPackageLocation, PackageLocation)]
+loadSnapshotLockFile = undefined
+
+generateSnapshotLockFile ::
+       Path Abs File -- ^ Snapshot file
+    -> [RawPackageLocation]
+    -> RIO Config ()
+generateSnapshotLockFile path rpl = do
+    logInfo "Generating Lock file for snapshot"
+    deps :: [PackageLocation] <- mapM completePackageLocation' rpl
+    lockFile <- liftIO $ addFileExtension "lock" path
+    let depPairs :: [(PackageLocation, RawPackageLocation)] = zip deps rpl
+        depsObject =
+            Yaml.object
+                [ ( "dependencies"
+                  , Yaml.array
+                        (map (\(raw, comp) ->
+                                  object
+                                      [ ("original", Yaml.toJSON raw)
+                                      , ("complete", Yaml.toJSON comp)
+                                      ])
+                             depPairs))
+                ]
+    B.writeFile (fromAbsFile lockFile) (Yaml.encode depsObject)
+
+-- Things to do
+-- 1. Creae function to write custom snapshot lock file. something like fn :: [RawPackageLocation] -> Path Abs File (snapshot file) -> IO ()
+-- 2. Create function to load data from custom snapshot file. Something like fn :: Path Abs File -> IO [(PackageLocation, RawPackageLocation)]
+-- 3. Try to use the loaded data in the loadBuildconfig function
+generateLockFileForCustomSnapshot ::
+       SnapshotLocation -> Path Abs File -> RIO Config ()
+generateLockFileForCustomSnapshot (SLFilePath path) stackFile
+    -- todo: see if there is existing and outdated file
+ = do
+    let snapshotPath = resolvedAbsolute path
+    rpl <- liftIO $ loadSnapshotFile snapshotPath (parent stackFile)
+    generateSnapshotLockFile snapshotPath rpl
+generateLockFileForCustomSnapshot xs _ = throwM (LockCannotGenerate xs)
+
+-- hasLockFile :: HasEnvConfig env => RIO env Bool
+-- hasLockFile = do
+--     bconfig <- view $ envConfigL . to envConfigBuildConfig
+--     let stackFile = bcStackYaml bconfig
+--     lockFile <- liftIO $ addFileExtension "lock" stackFile
+--     liftIO $ doesFileExist lockFile
 -- parsePLI :: HasEnvConfig env => BuildConfig -> RIO env [PackageLocation]
 -- parsePLI bconfig = do
 --     let stackFile = bcStackYaml bconfig
