1ac7ef3f2f79c34837d24a58cb56c0202391d893
David Baynard <davidbaynard@gmail.com>
2019-03-14 22:49:21 +0000

    Use defaultResolverArg

--- test/integration/tests/3827-skip-libs-bug/Main.hs
+++ test/integration/tests/3827-skip-libs-bug/Main.hs
@@ -3,16 +3,16 @@ import StackTest
 main :: IO ()
 main = do
 
-  stack ["clean", "--full"]
+  stack [defaultResolverArg, "clean", "--full"]
 
   -- Nothing should be built here
-  stack ["build", "--skip", "this", "--skip", "this-exe", "this"]
+  stack [defaultResolverArg, "build", "--skip", "this", "--skip", "this-exe", "this"]
   doesNotExist "./that/.stack-work"
   doesNotExist "./this/.stack-work"
 
   -- "That" should be built, but "this" should be skipped
-  stack ["build", "--skip", "this", "--skip", "this-exe"]
-  stack ["test", "--skip", "this-test", "--skip", "this-exe", "--skip", "this"]
+  stack [defaultResolverArg, "build", "--skip", "this", "--skip", "this-exe"]
+  stack [defaultResolverArg, "test", "--skip", "this-test", "--skip", "this-exe", "--skip", "this"]
 
   doesExist "./that/.stack-work"
   doesNotExist "./this/.stack-work"
