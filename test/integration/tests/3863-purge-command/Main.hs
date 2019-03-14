911c69ff04805122d69de9b6668e651d8c6159c9
Sibi Prabakaran <sibi@psibi.in>
2019-03-14 16:36:46 +0530

    Fix 3863-purge-command integration failure

--- test/integration/tests/3863-purge-command/Main.hs
+++ test/integration/tests/3863-purge-command/Main.hs
@@ -2,15 +2,19 @@ import StackTest
 import Data.Maybe (listToMaybe, fromMaybe)
 import System.Directory
 import System.FilePath
+import Data.List (filter)
+
+newline :: Char
+newline = '\n'
 
 main :: IO ()
 main =
   -- For these commands, we'll need to know the `dist` directory.
   -- This is usually `.stack-work/dist/$compiler-variant/Cabal-xxxx`
-  stackCheckStdout [defaultResolverArg, "path", "--dist-dir"] $ \distDir ->
-
-    stackCheckStdout [defaultResolverArg, "path", "--local-install-root"] $ \localInstallRoot -> do
-
+  stackCheckStdout [defaultResolverArg, "path", "--dist-dir"] $ \distDir' -> do
+    let distDir = filter (\x -> x /= newline) distDir'
+    stackCheckStdout [defaultResolverArg, "path", "--local-install-root"] $ \localInstallRoot' -> do
+      let localInstallRoot = filter (\x -> x /= newline) localInstallRoot'
       -- Usually `.stack-work`
       let stackWork = fromMaybe (error "There must be a stack working directory.") $
             listToMaybe (splitDirectories distDir)
@@ -19,7 +23,6 @@ main =
       -- This is only necessary when running individual tests.
       stack [defaultResolverArg, "purge"]
       doesNotExist stackWork
-
       -- The dist directory should exist after a build
       stack [defaultResolverArg, "build"]
       doesExist distDir
@@ -29,7 +32,6 @@ main =
       -- The dist directory should not exist after a clean, whereas the
       -- .stack-work directory should
       stack [defaultResolverArg, "clean"]
-      run "exa" ["-T", ".stack-work"]
       doesNotExist distDir
       doesExist localInstallRoot
       doesExist stackWork
