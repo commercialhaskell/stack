65c987c07cf41a9c43c156f3f8946f1e51a8892a
Michael Snoyman <michael@snoyman.com>
2019-04-15 22:35:43 +0300

    More integration test updates for formatting

--- test/integration/tests/proper-rebuilds/Main.hs
+++ test/integration/tests/proper-rebuilds/Main.hs
@@ -6,10 +6,10 @@ import System.Directory
 main :: IO ()
 main = do
     let expectRecompilation stderr =
-          unless ("files-1.0.0: build" `isInfixOf` stderr) $
+          unless ("> build" `isInfixOf` stderr) $
           error "package recompilation was expected"
         expectNoRecompilation stderr =
-          when ("files-1.0.0: build" `isInfixOf` stderr) $
+          when ("> build" `isInfixOf` stderr) $
           error "package recompilation was not expected"
     copyFile "src/Lib.hs.v1" "src/Lib.hs"
     stackCheckStderr ["build"] expectRecompilation
