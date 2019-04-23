3aeaceccde99c1981ca3c2dc28a5bebfb9ebe5a0
Michael Snoyman <michael@snoyman.com>
2019-04-23 17:24:27 +0300

    Switch from missiles to dont to avoid stm test failure

--- test/integration/tests/1438-configure-options/Main.hs
+++ test/integration/tests/1438-configure-options/Main.hs
@@ -12,7 +12,7 @@ main = do
       unless ("this is an invalid option" `isInfixOf` str) $
       error "Configure option is not present"
 
-  stack ["build", "--stack-yaml", "stack-locals.yaml", "acme-missiles"]
-  stack ["build", "--stack-yaml", "stack-targets.yaml", "acme-missiles"]
-  stackErr ["build", "--stack-yaml", "stack-name.yaml", "acme-missiles"]
-  stackErr ["build", "--stack-yaml", "stack-everything.yaml", "acme-missiles"]
+  stack ["build", "--stack-yaml", "stack-locals.yaml", "acme-dont"]
+  stack ["build", "--stack-yaml", "stack-targets.yaml", "acme-dont"]
+  stackErr ["build", "--stack-yaml", "stack-name.yaml", "acme-dont"]
+  stackErr ["build", "--stack-yaml", "stack-everything.yaml", "acme-dont"]
