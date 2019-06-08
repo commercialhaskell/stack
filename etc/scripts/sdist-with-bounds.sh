f612ea85316bbc327a64e4ad8d9f0b150dc12d4b
Emanuel Borsboom <manny@fpcomplete.com>
2019-06-08 15:01:49 -0700

    sdist-with-bounds.sh: support sub-packages

--- etc/scripts/sdist-with-bounds.sh
+++ etc/scripts/sdist-with-bounds.sh
@@ -2,44 +2,45 @@
 #TODO: move this logic into `stack --pvp-bounds`
 set -eu -o pipefail
 
+rootdir="$(cd "$(dirname "$0")/../.."; pwd)"
 stackbin=stack
-loweryaml=stack.yaml
-if [[ -f stack-lts-12.yaml ]]; then
-  midyaml=stack-lts-12.yaml
+loweryaml=$rootdir/stack.yaml
+if [[ -f $rootdir/stack-lts-12.yaml ]]; then
+  midyaml=$rootdir/stack-lts-12.yaml
 else
-  midyaml=stack.yaml
+  midyaml=$rootdir/stack.yaml
 fi
-if [[ -f stack-nightly.yaml ]]; then
-  upperyaml=stack-nightly.yaml
+if [[ -f $rootdir/stack-nightly.yaml ]]; then
+  upperyaml=$rootdir/stack-nightly.yaml
 else
-  upperyaml=stack.yaml
+  upperyaml=$rootdir/stack.yaml
 fi
 pkgyaml=package.yaml
 pkgyamlorig=$pkgyaml.SDIST-ORIG
 
-rm -f stack.cabal
-
-ver="$(grep '^version: ' "$pkgyaml"|sed "s/^version: '\(.*\)'$/\1/")"
+pkgver="$(grep '^version:' "$pkgyaml"|sed "s/^version: *'*\([^']*\)'*$/\1/")"
+pkgname="$(grep '^name:' "$pkgyaml"|sed "s/^name: *'*\([^']*\)'*$/\1/")"
 defaultdist="$("$stackbin" path --dist-dir)"
 lowerdist="$("$stackbin" --stack-yaml="$loweryaml" path --dist-dir)"
 middist="$("$stackbin" --stack-yaml="$midyaml" path --dist-dir)"
 upperdist="$("$stackbin" --stack-yaml="$upperyaml" path --dist-dir)"
 
-rm -f "$lowerdist/stack-$ver.tar.gz"
+rm -f $pkgname.cabal
+rm -f "$lowerdist/$pkgname-$pkgver.tar.gz"
 "$stackbin" --stack-yaml="$loweryaml" sdist . --pvp-bounds=both
-rm -rf "$lowerdist/_lower" && mkdir "$lowerdist/_lower" && (cd "$lowerdist/_lower" && tar xf "../stack-$ver.tar.gz")
-lcabal="$(echo "$lowerdist/_lower/stack-$ver/stack.cabal")"
-rm -f "$middist/stack-$ver.tar.gz"
+rm -rf "$lowerdist/_lower" && mkdir "$lowerdist/_lower" && (cd "$lowerdist/_lower" && tar xf "../$pkgname-$pkgver.tar.gz")
+lcabal="$(echo "$lowerdist/_lower/$pkgname-$pkgver/$pkgname.cabal")"
+rm -f "$middist/$pkgname-$pkgver.tar.gz"
 "$stackbin" --stack-yaml="$midyaml" sdist . --pvp-bounds=both
-rm -rf "$middist/_mid" && mkdir "$middist/_mid" && (cd "$middist/_mid" && tar xf "../stack-$ver.tar.gz")
-mcabal="$(echo "$middist/_mid/stack-$ver/stack.cabal")"
-rm -f "$upperdist/stack-$ver.tar.gz"
+rm -rf "$middist/_mid" && mkdir "$middist/_mid" && (cd "$middist/_mid" && tar xf "../$pkgname-$pkgver.tar.gz")
+mcabal="$(echo "$middist/_mid/$pkgname-$pkgver/$pkgname.cabal")"
+rm -f "$upperdist/$pkgname-$pkgver.tar.gz"
 "$stackbin" --stack-yaml="$upperyaml" sdist . --pvp-bounds=both
-rm -rf "$upperdist/_upper" && mkdir "$upperdist/_upper" && (cd "$upperdist/_upper" && tar xf "../stack-$ver.tar.gz")
-ucabal="$(echo "$upperdist/_upper/stack-$ver/stack.cabal")"
+rm -rf "$upperdist/_upper" && mkdir "$upperdist/_upper" && (cd "$upperdist/_upper" && tar xf "../$pkgname-$pkgver.tar.gz")
+ucabal="$(echo "$upperdist/_upper/$pkgname-$pkgver/$pkgname.cabal")"
 
 mv "$pkgyaml" "$pkgyamlorig"
-trap 'mv "$pkgyamlorig" "$pkgyaml"; rm -f stack.cabal; rm -f "$pkgyaml.sdist-bak"' EXIT
+trap 'mv "$pkgyamlorig" "$pkgyaml"; rm -f $pkgname.cabal; rm -f "$pkgyaml.sdist-bak"' EXIT
 cp "$pkgyamlorig" "$pkgyaml"
 
 while read -r line; do
@@ -61,8 +62,8 @@ done <"$lcabal"
 
 "$stackbin" sdist .
 echo
-cp "$defaultdist/stack-$ver.tar.gz" "_release/stack-$ver-sdist-1.tar.gz"
-echo "SDIST COPIED TO: _release/stack-$ver-sdist-1.tar.gz"
-rm -rf "$defaultdist/_both" && mkdir "$defaultdist/_both" && (cd "$defaultdist/_both" && tar xf "../stack-$ver.tar.gz")
-cp "$defaultdist/_both/stack-$ver/stack.cabal" "_release/stack-${ver}_bounds.cabal"
-echo "CABAL SPEC COPIED TO: _release/stack-${ver}_bounds.cabal"
+cp "$defaultdist/$pkgname-$pkgver.tar.gz" "$rootdir/_release/$pkgname-$pkgver-sdist-1.tar.gz"
+echo "SDIST COPIED TO: $rootdir/_release/$pkgname-$pkgver-sdist-1.tar.gz"
+rm -rf "$defaultdist/_both" && mkdir "$defaultdist/_both" && (cd "$defaultdist/_both" && tar xf "../$pkgname-$pkgver.tar.gz")
+cp "$defaultdist/_both/$pkgname-$pkgver/$pkgname.cabal" "$rootdir/_release/$pkgname-${pkgver}_bounds.cabal"
+echo "CABAL SPEC COPIED TO: $rootdir/_release/$pkgname-${pkgver}_bounds.cabal"
