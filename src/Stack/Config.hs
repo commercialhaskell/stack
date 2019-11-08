868c31b735c183601745cc914ad19aa8e9cbc969
Chris Done <chrisdone@gmail.com>
2019-11-08 15:35:03 +0000

    Update to more recent casa/pantry

--- src/Stack/Config.hs
+++ src/Stack/Config.hs
@@ -375,7 +375,7 @@ configFromConfigMonoid
        hsc
        (maybe HpackBundled HpackCommand $ getFirst configMonoidOverrideHpack)
        clConnectionCount
-       defaultCasaPullURL
+       defaultCasaRepoPrefix
        defaultCasaMaxPerRequest
        (\configPantryConfig -> initUserStorage
          (configStackRoot </> relFileStorage)
