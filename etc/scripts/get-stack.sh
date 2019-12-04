33d25a4e1b4ff9baffeb17f89d8388a86c185f53
Sigmund Vestergaard <sigmund.vestergaard@ie.ibm.com>
2019-12-04 09:12:47 +0000

    Added support for RHEL Workstation

--- etc/scripts/get-stack.sh
+++ etc/scripts/get-stack.sh
@@ -454,7 +454,7 @@ GETDISTRO
     fedora)
       do_fedora_install "$VERSION"
       ;;
-    centos|rhel|redhatenterpriseserver)
+    centos|rhel|redhatenterpriseserver|redhatenterpriseworkstation)
       do_centos_install "$VERSION"
       ;;
     alpine)
