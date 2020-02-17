3a986280c712fe705bba1c3a4c145fa6fa62300a
Jeff Happily <goodboy96@live.com.my>
2020-02-17 13:19:00 +0800

    Remove terminal size and make some changes

--- src/unix/System/Terminal.hsc
+++ src/unix/System/Terminal.hsc
@@ -1,9 +1,67 @@
 module System.Terminal
 ( fixCodePage
+, getTerminalWidth
 , hIsTerminalDeviceOrMinTTY
 ) where
 
 import           RIO (MonadIO, Handle, hIsTerminalDevice)
+import           Foreign
+-- import           Foreign.C.Error
+import           Foreign.C.Types
+-- import           GHC.IO.FD (FD(FD, fdFD))
+-- import           GHC.IO.Handle.Internals (withHandle_)
+-- import           GHC.IO.Handle.Types (Handle, Handle__(Handle__, haDevice))
+-- import           System.Posix.Types (Fd(Fd))
+
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+
+-- #let alignment t = "%lu", (unsigned long)offsetof(struct {char x__; t (y__); }, y__)
+newtype WindowWidth = WindowWidth CUShort
+    deriving (Eq, Ord, Show)
+
+instance Storable WindowWidth where
+  sizeOf _ = (#size struct winsize)
+  alignment _ = (#alignment struct winsize)
+  peek p = WindowWidth <$> (#peek struct winsize, ws_col) p
+  poke p (WindowWidth w) = do
+    (#poke struct winsize, ws_col) p w
+
+
+-- fdSize :: Integral n => Fd -> IO (Maybe (Window n))
+-- fdSize (Fd fd) = with (CWin 0 0) $ \ws -> do
+--   throwErrnoIfMinus1 "ioctl" $
+--     ioctl fd (#const TIOCGWINSZ) ws
+--   CWin row col <- peek ws
+--   return . Just $ Window (fromIntegral row) (fromIntegral col)
+--  `catch`
+--   handler
+--  where
+--   handler :: IOError -> IO (Maybe (Window h))
+--   handler _ = return Nothing
+
+foreign import ccall "sys/ioctl.h ioctl"
+  ioctl :: CInt -> CInt -> Ptr WindowWidth -> IO CInt
+
+-- size :: Integral n => IO (Maybe (Window n))
+-- size = fdSize (Fd (#const STDOUT_FILENO))
+
+-- hSize :: Integral n => Handle -> IO (Maybe (Window n))
+-- hSize h = withHandle_ "hSize" h $ \Handle__ { haDevice = dev } ->
+--   case cast dev of
+--     Nothing -> return Nothing
+--     Just FD { fdFD = fd } -> fdSize (Fd fd)
+
+getTerminalWidth :: IO (Maybe Int)
+getTerminalWidth =
+    alloca $ \p -> do	
+        errno <- ioctl (#const STDOUT_FILENO) (#const TIOCGWINSZ) p	
+        if errno < 0	
+        then return Nothing	
+        else do	
+            WindowWidth w <- peek p	
+            return . Just . fromIntegral $ w
 
 fixCodePage :: x -> y -> a -> a
 fixCodePage _ _ = id
--- src/windows/System/Terminal.hs
+++ src/windows/System/Terminal.hs
@@ -9,6 +9,65 @@ import Distribution.Types.Version (mkVersion)
 import Stack.Prelude
 import System.Win32 (isMinTTYHandle, withHandleToHANDLE)
 import System.Win32.Console (setConsoleCP, setConsoleOutputCP, getConsoleCP, getConsoleOutputCP)
+import System.Console.Terminal.Common
+
+import Control.Monad
+import Data.Word
+import Foreign.Ptr
+import Foreign.Storable
+import Foreign.Marshal.Alloc
+import System.Exit
+import System.IO
+import System.Process
+
+type HANDLE = Ptr ()
+
+data CONSOLE_SCREEN_BUFFER_INFO
+
+sizeCONSOLE_SCREEN_BUFFER_INFO :: Int
+sizeCONSOLE_SCREEN_BUFFER_INFO = 22
+
+posCONSOLE_SCREEN_BUFFER_INFO_srWindow :: Int
+posCONSOLE_SCREEN_BUFFER_INFO_srWindow = 10 -- 4 x Word16 Left,Top,Right,Bottom
+
+c_STD_OUTPUT_HANDLE :: Word32
+c_STD_OUTPUT_HANDLE = -11
+
+foreign import stdcall unsafe "windows.h GetConsoleScreenBufferInfo"
+    c_GetConsoleScreenBufferInfo :: HANDLE -> Ptr CONSOLE_SCREEN_BUFFER_INFO -> IO Bool
+
+foreign import stdcall unsafe "windows.h GetStdHandle"
+    c_GetStdHandle :: Word32 -> IO HANDLE
+
+
+size :: Integral n => IO (Maybe (Window n))
+size = do
+    hdl <- c_GetStdHandle c_STD_OUTPUT_HANDLE
+    allocaBytes sizeCONSOLE_SCREEN_BUFFER_INFO $ \p -> do
+        b <- c_GetConsoleScreenBufferInfo hdl p
+        if not b
+            then do -- This could happen on Cygwin or MSYS
+                let stty = (shell "stty size") {
+                      std_in  = UseHandle stdin
+                    , std_out = CreatePipe
+                    }
+                (_, mbStdout, _, rStty) <- createProcess stty
+                exStty <- waitForProcess rStty
+                case exStty of
+                    ExitFailure _ -> return Nothing
+                    ExitSuccess ->
+                        maybe (return Nothing)
+                              (\hSize -> do
+                                  sizeStr <- hGetContents hSize
+                                  let [r, c] = map read $ words sizeStr :: [Int]
+                                  return $ Just $ Window (fromIntegral r) (fromIntegral c)
+                              )
+                              mbStdout
+            else do
+                [left,top,right,bottom] <- forM [0..3] $ \i -> do
+                    v <- peekByteOff p ((i*2) + posCONSOLE_SCREEN_BUFFER_INFO_srWindow)
+                    return $ fromIntegral (v :: Word16)
+                return $ Just $ Window (1+bottom-top) (1+right-left)
 
 -- | Set the code page for this process as necessary. Only applies to Windows.
 -- See: https://github.com/commercialhaskell/stack/issues/738
