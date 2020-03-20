ae971ca3acc914ccdd0f95ef3a8d80a3edeed9c3
Jeff Happily <goodboy96@live.com.my>
2020-03-21 00:05:15 +0800

    Remove cache-s3

--- .azure/script/build.sh
+++ .azure/script/build.sh
@@ -26,13 +26,11 @@ cd $dir
 export STACK_ROOT=$HOME/.stack-root;
 echo $STACK_ROOT
 mkdir -p $HOME/.local/bin
-echo "URL:"
-echo "https://github.com/fpco/cache-s3/releases/download/${cache_s3_version}/cache-s3-${cache_s3_version}-linux-x86_64.tar.gz"
-curl -f -L "https://github.com/fpco/cache-s3/releases/download/${cache_s3_version}/cache-s3-${cache_s3_version}-linux-x86_64.tar.gz" -o $HOME/.local/bin/cache-s3.tar.gz
-tar xzf $HOME/.local/bin/cache-s3.tar.gz -C $HOME/.local/bin
+# curl -f -L "https://github.com/fpco/cache-s3/releases/download/${cache_s3_version}/cache-s3-${cache_s3_version}-linux-x86_64.tar.gz" -o $HOME/.local/bin/cache-s3.tar.gz
+# tar xzf $HOME/.local/bin/cache-s3.tar.gz -C $HOME/.local/bin
 export PATH=$HOME/.local/bin:$PATH;
-cache-s3 --prefix="${cache_s3_prefix}" --git-branch="${source_branch}" --suffix="${os_name}" restore stack --base-branch="${base_branch}"
-cache-s3 --prefix="${cache_s3_prefix}" --git-branch="${source_branch}" --suffix="${os_name}" restore stack work --base-branch="${base_branch}"
+# cache-s3 --prefix="${cache_s3_prefix}" --git-branch="${source_branch}" --suffix="${os_name}" restore stack --base-branch="${base_branch}"
+# cache-s3 --prefix="${cache_s3_prefix}" --git-branch="${source_branch}" --suffix="${os_name}" restore stack work --base-branch="${base_branch}"
 etc/scripts/ci-setup.sh
 export PATH=$HOME/.local/bin:/opt/ghc/$GHCVER/bin:/opt/happy/1.19.5/bin:/opt/alex/3.1.7/bin:$PATH
 
@@ -40,10 +38,10 @@ set -ex
 stack etc/scripts/release.hs build
 set +ex
 
-if [ "${source_branch}" = "${base_branch}" ]; then
-    cache-s3 --prefix="${cache_s3_prefix}" --git-branch="${source_branch}" --suffix="${os_name}" save stack;
-fi;
-cache-s3 --prefix="${cache_s3_prefix}" --git-branch="${source_branch}" --suffix="${os_name}" save stack work
+# if [ "${source_branch}" = "${base_branch}" ]; then
+#     cache-s3 --prefix="${cache_s3_prefix}" --git-branch="${source_branch}" --suffix="${os_name}" save stack;
+# fi;
+# cache-s3 --prefix="${cache_s3_prefix}" --git-branch="${source_branch}" --suffix="${os_name}" save stack work
 
 # Reset env var
 export STACK_ROOT=$ori_stack_root
