779fb66b294ac63337054bf3f198a9409877896a
Michael Snoyman <michael@snoyman.com>
2020-06-24 08:18:59 +0300

    Fix integration test for random 1.2 being available

--- test/integration/tests/3431-precompiled-works/Main.hs
+++ test/integration/tests/3431-precompiled-works/Main.hs
@@ -4,7 +4,7 @@ import Data.List
 
 main :: IO ()
 main = do
-    stack ["build", "random", "--stack-yaml", "custom1/stack.yaml"]
-    stackCheckStderr ["build", "random", "--stack-yaml", "custom2/stack.yaml"] $ \out -> do
+    stack ["build", "random-1.1", "--stack-yaml", "custom1/stack.yaml"]
+    stackCheckStderr ["build", "random-1.1", "--stack-yaml", "custom2/stack.yaml"] $ \out -> do
       print out
       unless ("precompiled" `isInfixOf` out) $ error "Didn't use precompiled!"
