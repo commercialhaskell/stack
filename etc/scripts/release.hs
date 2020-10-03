11ec259e62821ba89b67abb6ed6ef7b0c72cbd27
Emanuel Borsboom <manny@fpcomplete.com>
2020-10-03 11:33:47 -0700

    Automate signing and uploading stack bindists to Github release
    Adds to Github Actions workflow, and removes from `release.hs`.

--- etc/scripts/release.hs
+++ etc/scripts/release.hs
@@ -28,8 +28,6 @@
 import Control.Applicative
 import Control.Exception
 import Control.Monad
-import Control.Monad.Trans.Resource (runResourceT)
-import qualified Data.ByteString.Char8 as S8
 import qualified Data.ByteString.Lazy.Char8 as L8
 import Data.List
 import Data.Maybe
@@ -40,7 +38,6 @@ import Distribution.Package
 import Distribution.PackageDescription hiding (options)
 import Distribution.Verbosity
 import System.Console.GetOpt
-import System.Environment
 import System.Directory
 import System.IO.Error
 import System.Process
@@ -48,18 +45,9 @@ import System.Process
 import qualified Codec.Archive.Tar as Tar
 import qualified Codec.Archive.Zip as Zip
 import qualified Codec.Compression.GZip as GZip
-import Crypto.Hash (Digest, SHA256 (..), digestToHexByteString, hash)
-import Data.Aeson
-import qualified Data.CaseInsensitive as CI
-import Data.Conduit
-import qualified Data.Conduit.Combinators as CC
 import Data.List.Extra
-import qualified Data.Text as T
 import Development.Shake
 import Development.Shake.FilePath
-import Network.HTTP.Conduit
-import Network.HTTP.Types
-import Network.Mime
 import Prelude -- Silence AMP warning
 
 -- | Entrypoint.
@@ -77,23 +65,18 @@ main =
             _ <- readProcess "stack" ["build", "--dry-run"] ""
             gStackPackageDescription <-
                 packageDescription <$> readGenericPackageDescription silent "stack.cabal"
-            gGpgKey <- maybe defaultGpgKey Just <$> lookupEnv gpgKeyEnvVar
-            gGithubAuthToken <- lookupEnv githubAuthTokenEnvVar
             gGitRevCount <- length . lines <$> readProcess "git" ["rev-list", "HEAD"] ""
             gGitSha <- trim <$> readProcess "git" ["rev-parse", "HEAD"] ""
             gHomeDir <- getHomeDirectory
 
             let gAllowDirty = False
-                gGithubReleaseTag = Nothing
                 Platform arch _ = buildPlatform
                 gArch = arch
                 gBinarySuffix = ""
-                gUploadLabel = Nothing
                 gTestHaddocks = True
                 gProjectRoot = "" -- Set to real value velow.
                 gBuildArgs = ["--flag", "stack:-developer-mode"]
                 gCertificateName = Nothing
-                gUploadOnly = False
                 global0 = foldl (flip id) Global{..} flags
 
             -- Need to get paths after options since the '--arch' argument can effect them.
@@ -109,21 +92,8 @@ main =
 -- | Additional command-line options.
 options :: [OptDescr (Either String (Global -> Global))]
 options =
-    [ Option "" [gpgKeyOptName]
-        (ReqArg (\v -> Right $ \g -> g{gGpgKey = Just v}) "USER-ID")
-        ("GPG user ID to sign distribution package with (defaults to " ++
-         gpgKeyEnvVar ++
-         " environment variable).")
-    , Option "" [allowDirtyOptName] (NoArg $ Right $ \g -> g{gAllowDirty = True})
+    [ Option "" [allowDirtyOptName] (NoArg $ Right $ \g -> g{gAllowDirty = True})
         "Allow a dirty working tree for release."
-    , Option "" [githubAuthTokenOptName]
-        (ReqArg (\v -> Right $ \g -> g{gGithubAuthToken = Just v}) "TOKEN")
-        ("Github personal access token (defaults to " ++
-         githubAuthTokenEnvVar ++
-         " environment variable).")
-    , Option "" [githubReleaseTagOptName]
-        (ReqArg (\v -> Right $ \g -> g{gGithubReleaseTag = Just v}) "TAG")
-        "Github release tag to upload to."
     , Option "" [archOptName]
         (ReqArg
             (\v -> case simpleParse v of
@@ -134,9 +104,6 @@ options =
     , Option "" [binaryVariantOptName]
         (ReqArg (\v -> Right $ \g -> g{gBinarySuffix = v}) "SUFFIX")
         "Extra suffix to add to binary executable archive filename."
-    , Option "" [uploadLabelOptName]
-        (ReqArg (\v -> Right $ \g -> g{gUploadLabel = Just v}) "LABEL")
-        "Label to give the uploaded release asset"
     , Option "" [noTestHaddocksOptName] (NoArg $ Right $ \g -> g{gTestHaddocks = False})
         "Disable testing building haddocks."
     , Option "" [alpineOptName]
@@ -153,8 +120,6 @@ options =
     , Option "" [certificateNameOptName]
         (ReqArg (\v -> Right $ \g -> g{gCertificateName = Just v}) "NAME")
         "Certificate name for code signing on Windows"
-    , Option "" [uploadOnlyOptName] (NoArg $ Right $ \g -> g{gUploadOnly = True})
-        "Just upload an existing file, but don't try to build it."
     ]
 
 -- | Shake rules.
@@ -166,7 +131,7 @@ rules global@Global{..} args = do
 
     phony releasePhony $ do
         need [checkPhony]
-        need [uploadPhony]
+        need [buildPhony]
 
     phony cleanPhony $
         removeFilesAfter releaseDir ["//*"]
@@ -174,29 +139,11 @@ rules global@Global{..} args = do
     phony checkPhony $
         need [releaseCheckDir </> binaryExeFileName]
 
-    phony uploadPhony $
-        mapM_ (\f -> need [releaseDir </> f <.> uploadExt]) binaryPkgAndSigFileNames
-
     phony buildPhony $
-        mapM_ (\f -> need [releaseDir </> f]) binaryPkgAndSigFileNames
-
-    releaseDir </> "*" <.> uploadExt %> \out -> do
-        let srcFile = dropExtension out
-            -- mUploadLabel =
-            --     case takeExtension srcFile of
-            --         e | e == ascExt -> fmap (++ " (GPG signature)") gUploadLabel
-            --           | e == sha256Ext -> fmap (++ " (SHA256 checksum)") gUploadLabel
-            --           | otherwise -> gUploadLabel
-        need [srcFile]
-        uploadToGithubRelease global srcFile Nothing
-        copyFileChanged srcFile out
+        mapM_ (\f -> need [releaseDir </> f]) binaryPkgFileNames
 
     releaseCheckDir </> binaryExeFileName %> \out -> do
         need [releaseBinDir </> binaryName </> stackExeFileName]
-
-        -- Run "git diff" so we can see what changes exist, in case things fail below
-        () <- cmd "git diff"
-
         Stdout dirty <- cmd "git status --porcelain"
         when (not gAllowDirty && not (null (trim dirty))) $
             error $ concat
@@ -220,7 +167,7 @@ rules global@Global{..} args = do
             [gProjectRoot </> releaseBinDir </> binaryName </> "stack-integration-test"]
         copyFileChanged (releaseBinDir </> binaryName </> stackExeFileName) out
 
-    unless gUploadOnly $ releaseDir </> binaryPkgZipFileName %> \out -> do
+    releaseDir </> binaryPkgZipFileName %> \out -> do
         stageFiles <- getBinaryPkgStageFiles
         putNormal $ "zip " ++ out
         liftIO $ do
@@ -237,7 +184,7 @@ rules global@Global{..} args = do
             let archive = foldr Zip.addEntryToArchive Zip.emptyArchive entries
             L8.writeFile out (Zip.fromArchive archive)
 
-    unless gUploadOnly $ releaseDir </> binaryPkgTarGzFileName %> \out -> do
+    releaseDir </> binaryPkgTarGzFileName %> \out -> do
         stageFiles <- getBinaryPkgStageFiles
         writeTarGz out releaseStageDir stageFiles
 
@@ -249,10 +196,9 @@ rules global@Global{..} args = do
             (dropDirectoryPrefix (releaseStageDir </> binaryName) out)
             out
 
-    unless gUploadOnly $ releaseDir </> binaryExeFileName %> \out -> do
+    releaseDir </> binaryExeFileName %> \out -> do
         need [releaseBinDir </> binaryName </> stackExeFileName]
         (Stdout versionOut) <- cmd (releaseBinDir </> binaryName </> stackExeFileName) "--version"
-        () <- cmd "git diff"
         when (not gAllowDirty && "dirty" `isInfixOf` lower versionOut) $
             error ("Refusing continue because 'stack --version' reports dirty.  Use --" ++
                    allowDirtyOptName ++ " option to continue anyway.")
@@ -291,33 +237,11 @@ rules global@Global{..} args = do
 
     releaseDir </> binaryInstallerNSIFileName %> \out -> do
         need ["etc" </> "scripts" </> "build-stack-installer" <.> "hs"]
-        cmd "stack etc/scripts/build-stack-installer.hs" 
+        cmd "stack etc/scripts/build-stack-installer.hs"
             [ binaryExeFileName
             , binaryInstallerFileName
             , out
             ] :: Action ()
-
-    releaseDir </> "*" <.> ascExt %> \out -> do
-        need [out -<.> ""]
-        _ <- liftIO $ tryJust (guard . isDoesNotExistError) (removeFile out)
-        case gGpgKey of
-            Nothing -> error "No GPG key specified"
-            Just gpgKey ->
-                cmd ("gpg " ++ gpgOptions ++ " --detach-sig --armor")
-                    [ "-u", gpgKey
-                    , dropExtension out ]
-
-    releaseDir </> "*" <.> sha256Ext %> \out -> do
-        need [out -<.> ""]
-        bs <- liftIO $ do
-            _ <- tryJust (guard . isDoesNotExistError) (removeFile out)
-            S8.readFile (dropExtension out)
-        writeFileChanged
-          out
-          ( S8.unpack (digestToHexByteString (hash bs :: Digest SHA256)) ++
-            "  " ++
-            takeFileName (dropExtension out) ++
-            "\n" )
 
     releaseBinDir </> binaryName </> stackExeFileName %> \out -> do
         alwaysRerun
@@ -354,7 +278,6 @@ rules global@Global{..} args = do
 
     releasePhony = "release"
     checkPhony = "check"
-    uploadPhony = "upload"
     cleanPhony = "clean"
     buildPhony = "build"
 
@@ -362,12 +285,6 @@ rules global@Global{..} args = do
     releaseStageDir = releaseDir </> "stage"
     releaseBinDir = releaseDir </> "bin"
 
-    binaryPkgAndSigFileNames =
-        concatMap sigHashFileNames binaryPkgFileNames
-    sigHashFileNames x =
-        case gGpgKey of
-            Nothing -> [x, x <.> sha256Ext]
-            Just _ -> [x, x <.> sha256Ext, x <.> ascExt]
     binaryPkgFileNames =
         case platformOS of
             Windows -> [binaryExeFileName, binaryPkgZipFileName, binaryPkgTarGzFileName, binaryInstallerFileName]
@@ -394,77 +311,7 @@ rules global@Global{..} args = do
     tarGzExt = tarExt <.> gzExt
     gzExt = ".gz"
     tarExt = ".tar"
-    ascExt = ".asc"
-    sha256Ext = ".sha256"
-    uploadExt = ".upload"
     nsiExt = ".nsi"
-
--- | Upload file to Github release.
-uploadToGithubRelease :: Global -> FilePath -> Maybe String -> Action ()
-uploadToGithubRelease global@Global{..} file mUploadLabel = do
-    -- TODO: consider using https://github.com/tfausak/github-release
-    putNormal $ "Uploading to Github: " ++ file
-    GithubRelease{..} <- getGithubRelease
-    resp <- liftIO $ callGithubApi global
-        [(CI.mk $ S8.pack "Content-Type", defaultMimeLookup (T.pack file))]
-        (Just file)
-        (replace
-            "{?name,label}"
-            ("?name=" ++ urlEncodeStr (takeFileName file) ++
-             (case mUploadLabel of
-                 Nothing -> ""
-                 Just uploadLabel -> "&label=" ++ urlEncodeStr uploadLabel))
-            relUploadUrl)
-    case eitherDecode resp of
-        Left e -> error ("Could not parse Github asset upload response (" ++ e ++ "):\n" ++ L8.unpack resp ++ "\n")
-        Right (GithubReleaseAsset{..}) ->
-            when (assetState /= "uploaded") $
-                error ("Invalid asset state after Github asset upload: " ++ assetState)
-  where
-    urlEncodeStr = S8.unpack . urlEncode True . S8.pack
-    getGithubRelease = do
-        releases <- getGithubReleases
-        let tag = fromMaybe ("v" ++ stackVersionStr global) gGithubReleaseTag
-        return $ fromMaybe
-            (error ("Could not find Github release with tag '" ++ tag ++ "'.\n" ++
-                    "Use --" ++ githubReleaseTagOptName ++ " option to specify a different tag."))
-            (find (\r -> relTagName r == tag) releases)
-    getGithubReleases :: Action [GithubRelease]
-    getGithubReleases = do
-        resp <- liftIO $ callGithubApi global
-            [] Nothing "https://api.github.com/repos/commercialhaskell/stack/releases"
-        case eitherDecode resp of
-            Left e -> error ("Could not parse Github releases (" ++ e ++ "):\n" ++ L8.unpack resp ++ "\n")
-            Right r -> return r
-
--- | Make a request to the Github API and return the response.
-callGithubApi :: Global -> RequestHeaders -> Maybe FilePath -> String -> IO L8.ByteString
-callGithubApi Global{..} headers mpostFile url = do
-    req0 <- parseUrlThrow url
-    let authToken =
-            fromMaybe
-                (error $
-                     "Github auth token required.\n" ++
-                     "Use " ++ githubAuthTokenEnvVar ++ " environment variable\n" ++
-                     "or --" ++ githubAuthTokenOptName ++ " option to specify.")
-                gGithubAuthToken
-        req1 =
-            req0
-                { requestHeaders =
-                    [ (CI.mk $ S8.pack "Authorization", S8.pack $ "token " ++ authToken)
-                    , (CI.mk $ S8.pack "User-Agent", S8.pack "commercialhaskell/stack") ] ++
-                    headers }
-    req <- case mpostFile of
-        Nothing -> return req1
-        Just postFile -> do
-            lbs <- L8.readFile postFile
-            return $ req1
-                { method = S8.pack "POST"
-                , requestBody = RequestBodyLBS lbs }
-    manager <- newManager tlsManagerSettings
-    runResourceT $ do
-        res <- http req manager
-        runConduit $ responseBody res .| CC.sinkLazy
 
 -- | Create a .tar.gz files from files.  The paths should be absolute, and will
 -- be made relative to the base directory in the tarball.
@@ -496,30 +343,6 @@ platformOS =
 releaseDir :: FilePath
 releaseDir = "_release"
 
--- | @GITHUB_AUTH_TOKEN@ environment variable name.
-githubAuthTokenEnvVar :: String
-githubAuthTokenEnvVar = "GITHUB_AUTH_TOKEN"
-
--- | @--github-auth-token@ command-line option name.
-githubAuthTokenOptName :: String
-githubAuthTokenOptName = "github-auth-token"
-
--- | @--github-release-tag@ command-line option name.
-githubReleaseTagOptName :: String
-githubReleaseTagOptName = "github-release-tag"
-
--- | Default GPG key ID for signing bindists
-defaultGpgKey :: Maybe String
-defaultGpgKey = Nothing
-
--- | @STACK_RELEASE_GPG_KEY@ environment variable name.
-gpgKeyEnvVar :: String
-gpgKeyEnvVar =  "STACK_RELEASE_GPG_KEY"
-
--- | @--gpg-key@ command-line option name.
-gpgKeyOptName :: String
-gpgKeyOptName = "gpg-key"
-
 -- | @--allow-dirty@ command-line option name.
 allowDirtyOptName :: String
 allowDirtyOptName = "allow-dirty"
@@ -531,10 +354,6 @@ archOptName = "arch"
 -- | @--binary-variant@ command-line option name.
 binaryVariantOptName :: String
 binaryVariantOptName = "binary-variant"
-
--- | @--upload-label@ command-line option name.
-uploadLabelOptName :: String
-uploadLabelOptName = "upload-label"
 
 -- | @--no-test-haddocks@ command-line option name.
 noTestHaddocksOptName :: String
@@ -552,10 +371,6 @@ alpineOptName = "alpine"
 certificateNameOptName :: String
 certificateNameOptName = "certificate-name"
 
--- | @--upload-only@ command-line option name.
-uploadOnlyOptName :: String
-uploadOnlyOptName = "upload-only"
-
 -- | Arguments to pass to all 'stack' invocations.
 stackArgs :: Global -> [String]
 stackArgs Global{..} = ["--arch=" ++ display gArch, "--interleaved-output"]
@@ -564,53 +379,24 @@ stackArgs Global{..} = ["--arch=" ++ display gArch, "--interleaved-output"]
 stackProgName :: FilePath
 stackProgName = "stack"
 
--- | Options to pass to invocations of gpg
-gpgOptions :: String
-gpgOptions = "--digest-algo=sha512"
-
 -- | Linux distribution/version combination.
 data DistroVersion = DistroVersion
     { dvDistro :: !String
     , dvVersion :: !String
     , dvCodeName :: !String }
 
--- | A Github release, as returned by the Github API.
-data GithubRelease = GithubRelease
-    { relUploadUrl :: !String
-    , relTagName :: !String }
-    deriving (Show)
-instance FromJSON GithubRelease where
-    parseJSON = withObject "GithubRelease" $ \o ->
-        GithubRelease
-        <$> o .: T.pack "upload_url"
-        <*> o .: T.pack "tag_name"
-
--- | A Github release asset, as returned by the Github API.
-data GithubReleaseAsset = GithubReleaseAsset
-    { assetState :: !String }
-    deriving (Show)
-instance FromJSON GithubReleaseAsset where
-    parseJSON = withObject "GithubReleaseAsset" $ \o ->
-        GithubReleaseAsset
-        <$> o .: T.pack "state"
-
 -- | Global values and options.
 data Global = Global
     { gStackPackageDescription :: !PackageDescription
-    , gGpgKey :: !(Maybe String)
     , gAllowDirty :: !Bool
-    , gGithubAuthToken :: !(Maybe String)
-    , gGithubReleaseTag :: !(Maybe String)
     , gGitRevCount :: !Int
     , gGitSha :: !String
     , gProjectRoot :: !FilePath
     , gHomeDir :: !FilePath
     , gArch :: !Arch
     , gBinarySuffix :: !String
-    , gUploadLabel :: !(Maybe String)
     , gTestHaddocks :: !Bool
     , gBuildArgs :: [String]
     , gCertificateName :: !(Maybe String)
-    , gUploadOnly :: !Bool
     }
     deriving (Show)
