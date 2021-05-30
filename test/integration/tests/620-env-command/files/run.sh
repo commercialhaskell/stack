a4230c84f50d19b5c9f0ba0061fdd802e30308db
Michael Snoyman <michael@snoyman.com>
2021-05-30 05:28:14 +0300

    Update resolvers

--- test/integration/tests/620-env-command/files/run.sh
+++ test/integration/tests/620-env-command/files/run.sh
@@ -2,6 +2,6 @@
 
 set -euxo pipefail
 
-stack build --resolver lts-14.27 async
-eval `stack config env --resolver lts-14.27`
+stack build --resolver lts-17.10 async
+eval `stack config env --resolver lts-17.10`
 ghc Main.hs
