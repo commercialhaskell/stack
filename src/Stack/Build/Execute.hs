7617fe5ac6b1fb66aa06ded8fbb72739c744590b
Matt Revelle <mrevelle@gmail.com>
2021-08-26 19:01:27 -0600

    Fix for bug where a local package that has been built from a local consumer would be installed in the package db for the consumer project but not its own package db.

--- src/Stack/Build/Execute.hs
+++ src/Stack/Build/Execute.hs
@@ -907,11 +907,6 @@ ensureConfig newConfigCache pkgDir ExecuteEnv {..} announce cabal cabalfp task =
             TTLocalMutable{} -> writeConfigCache pkgDir newConfigCache
             TTRemotePackage{} -> return ()
         writeCabalMod pkgDir newCabalMod
-        -- This file gets updated one more time by the configure step, so get
-        -- the most recent value. We could instead change our logic above to
-        -- check if our config mod file is newer than the file above, but this
-        -- seems reasonable too.
-        getNewSetupConfigMod >>= writeSetupConfigMod pkgDir
 
     return needConfig
   where
