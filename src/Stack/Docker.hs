3437aa3097a64024b3d1e984eb8a0e125dfffb59
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2022-04-08 22:02:37 +0100

    Remove unnecessary CPP directives
    `stack.yaml` currently uses resolver `lts-17.15`, which provides `path-0.7.0`.
    
    This pull request removes unnecessary `#if MIN_VERSION_path(0,7,0)`.

--- src/Stack/Docker.hs
+++ src/Stack/Docker.hs
@@ -154,14 +154,10 @@ getCmdArgs docker imageInfo isRemoteDocker = do
         -- MSS 2020-04-21 previously used replaceExtension, but semantics changed in path 0.7
         -- In any event, I'm not even sure _why_ we need to drop a file extension here
         -- Originally introduced here: https://github.com/commercialhaskell/stack/commit/6218dadaf5fd7bf312bb1bd0db63b4784ba78cb2
-#if MIN_VERSION_path(0, 7, 0)
         let exeBase =
               case splitExtension exePath of
                 Left _ -> exePath
                 Right (x, _) -> x
-#else
-        exeBase <- exePath -<.> ""
-#endif
         let mountPath = hostBinDir FP.</> toFilePath (filename exeBase)
         return (mountPath, args, [], [Mount (toFilePath exePath) mountPath])
 
