569aa6a22dc3f68533c5a86a5eea79a55e6b0ee4
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-09-07 19:43:06 -0400

    spelling: ambiguity
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/Stack/Options/Utils.hs
+++ src/Stack/Options/Utils.hs
@@ -10,7 +10,7 @@ hideMods :: Bool -> Mod f a
 hideMods hide = if hide then internal <> hidden else idm
 
 -- | Allows adjust global options depending on their context
--- Note: This was being used to remove ambibuity between the local and global
+-- Note: This was being used to remove ambiguity between the local and global
 -- implementation of stack init --resolver option. Now that stack init has no
 -- local --resolver this is not being used anymore but the code is kept for any
 -- similar future use cases.
