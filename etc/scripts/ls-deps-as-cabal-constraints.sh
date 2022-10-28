4fe0c851c2bf4a7f5f65f547751edcf4109c337f
Phil de Joux <philderbeast@gmail.com>
2022-10-28 11:38:06 -0400

    Leave a comment in the cabal constraints script.

--- etc/scripts/ls-deps-as-cabal-constraints.sh
+++ etc/scripts/ls-deps-as-cabal-constraints.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+# Lists the dependencies as exact (==) Cabal constraints for use with a cabal
+# project:
+#  constraints:
+#    , Cabal ==3.6.3.0
+#    , Cabal-syntax ==3.6.0.0
+#    , Glob ==0.10.2
 set -eu
 
 stack run -- ls dependencies cabal > "$1"
