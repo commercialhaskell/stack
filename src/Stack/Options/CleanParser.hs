98734d2177c33cdd23424cc7fb0ee0f4ab4f82af
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2022-12-04 22:11:06 +0000

    Add explicit import lists to Stack.Options.CleanParser
    Also, some reformatting.

--- src/Stack/Options/CleanParser.hs
+++ src/Stack/Options/CleanParser.hs
@@ -1,25 +1,28 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 
-module Stack.Options.CleanParser where
+module Stack.Options.CleanParser
+  ( cleanOptsParser
+  ) where
 
-import           Options.Applicative
-import           Stack.Clean                       (CleanCommand(..), CleanOpts (..))
+import           Options.Applicative ( Parser, flag', help, long, metavar )
+import           Stack.Clean ( CleanCommand (..), CleanOpts (..) )
 import           Stack.Prelude
-import           Stack.Types.PackageName
+import           Stack.Types.PackageName ( packageNameArgument )
 
 -- | Command-line parser for the clean command.
 cleanOptsParser :: CleanCommand -> Parser CleanOpts
-cleanOptsParser Clean = CleanShallow <$> packages <|> doFullClean
-  where
-    packages =
-        many
-            (packageNameArgument
-                 (metavar "PACKAGE" <>
-                  help "If none specified, clean all project packages"))
-    doFullClean =
-        flag'
-            CleanFull
-            (long "full" <>
-             help "Delete the project’s Stack working directories (.stack-work by default).")
+cleanOptsParser Clean = CleanShallow
+  <$> packages
+  <|> doFullClean
+ where
+  packages = many (packageNameArgument
+    (  metavar "PACKAGE"
+    <> help "If none specified, clean all project packages"
+    ))
+  doFullClean = flag' CleanFull
+    (  long "full"
+    <> help "Delete the project's Stack working directories (.stack-work by \
+            \default)."
+    )
 
 cleanOptsParser Purge = pure CleanFull
