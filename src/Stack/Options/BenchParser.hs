37520d7ef70dbf18eacf0316ab2b42c156e86e76
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2022-12-04 21:59:35 +0000

    Add explicit import list to Options.BenchParser
    Also, some reformatting

--- src/Stack/Options/BenchParser.hs
+++ src/Stack/Options/BenchParser.hs
@@ -1,25 +1,34 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module Stack.Options.BenchParser where
+module Stack.Options.BenchParser
+ ( benchOptsParser
+ ) where
 
 import           Options.Applicative
-import           Options.Applicative.Builder.Extra
+                   ( Parser, flag', help, long, metavar, strOption )
+import           Options.Applicative.Builder.Extra ( optionalFirst )
 import           Stack.Prelude
-import           Stack.Options.Utils
-import           Stack.Types.Config
+import           Stack.Options.Utils ( hideMods )
+import           Stack.Types.Config ( BenchmarkOptsMonoid (..) )
 
 -- | Parser for bench arguments.
 -- FIXME hiding options
 benchOptsParser :: Bool -> Parser BenchmarkOptsMonoid
 benchOptsParser hide0 = BenchmarkOptsMonoid
-        <$> optionalFirst (strOption (long "benchmark-arguments" <>
-                                      long "ba" <>
-                                 metavar "BENCH_ARGS" <>
-                                 help ("Forward BENCH_ARGS to the benchmark suite. " <>
-                                       "Supports templates from `cabal bench`") <>
-                                 hide))
-        <*> optionalFirst (flag' True (long "no-run-benchmarks" <>
-                          help "Disable running of benchmarks. (Benchmarks will still be built.)" <>
-                             hide))
-   where hide = hideMods hide0
+  <$> optionalFirst (strOption
+        (  long "benchmark-arguments"
+        <> long "ba"
+        <> metavar "BENCH_ARGS"
+        <> help "Forward BENCH_ARGS to the benchmark suite. Supports templates \
+                \from `cabal bench`"
+        <> hide
+        ))
+  <*> optionalFirst (flag' True
+        (  long "no-run-benchmarks"
+        <> help "Disable running of benchmarks. (Benchmarks will still be \
+                \built.)"
+        <> hide
+        ))
+ where
+  hide = hideMods hide0
