ce754a358ef0a1934374a23febdd5bb0840e3f4f
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2022-12-10 00:14:29 +0000

    Add export lists and explicit imports
    Also, some reformatting of the same modules.

--- src/Stack/Options/ConfigParser.hs
+++ src/Stack/Options/ConfigParser.hs
@@ -1,192 +1,211 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 
-module Stack.Options.ConfigParser where
+module Stack.Options.ConfigParser
+  ( configOptsParser
+  ) where
 
-import           Data.Char
+import           Data.Char ( toUpper )
 import           Options.Applicative
+                   ( Parser, auto, completer, completeWith, eitherReader, help
+                   , long, metavar, option, short, strOption
+                   )
 import           Options.Applicative.Builder.Extra
-import           Path
-import           Stack.Constants
-import           Stack.Options.BuildMonoidParser
-import           Stack.Options.DockerParser
-import           Stack.Options.GhcBuildParser
-import           Stack.Options.GhcVariantParser
-import           Stack.Options.NixParser
-import           Stack.Options.Utils
+                   ( PathCompleterOpts (..), absDirOption, absFileOption
+                   , defaultPathCompleterOpts, dirCompleter, firstBoolFlagsFalse
+                   , firstBoolFlagsNoDefault, firstBoolFlagsTrue, optionalFirst
+                   , pathCompleterWith
+                   )
+import           Path ( parseRelDir )
+import           Stack.Constants ( stackRootOptionName )
+import           Stack.Options.BuildMonoidParser ( buildOptsMonoidParser )
+import           Stack.Options.DockerParser ( dockerOptsParser )
+import           Stack.Options.GhcBuildParser ( ghcBuildParser )
+import           Stack.Options.GhcVariantParser ( ghcVariantParser )
+import           Stack.Options.NixParser ( nixOptsParser )
+import           Stack.Options.Utils ( GlobalOptsContext (..), hideMods )
 import           Stack.Prelude
 import           Stack.Types.Config
+                   ( ConfigMonoid (..), DumpLogs (..), readColorWhen )
 import qualified System.FilePath as FilePath
 
 -- | Command-line arguments parser for configuration.
 configOptsParser :: FilePath -> GlobalOptsContext -> Parser ConfigMonoid
 configOptsParser currentDir hide0 =
-    (\stackRoot workDir buildOpts dockerOpts nixOpts systemGHC installGHC arch
-        ghcVariant ghcBuild jobs includes libs preprocs overrideGccPath overrideHpack
-        skipGHCCheck skipMsys localBin setupInfoLocations modifyCodePage
-        allowDifferentUser dumpLogs colorWhen snapLoc noRunCompile -> mempty
-            { configMonoidStackRoot = stackRoot
-            , configMonoidWorkDir = workDir
-            , configMonoidBuildOpts = buildOpts
-            , configMonoidDockerOpts = dockerOpts
-            , configMonoidNixOpts = nixOpts
-            , configMonoidSystemGHC = systemGHC
-            , configMonoidInstallGHC = installGHC
-            , configMonoidSkipGHCCheck = skipGHCCheck
-            , configMonoidArch = arch
-            , configMonoidGHCVariant = ghcVariant
-            , configMonoidGHCBuild = ghcBuild
-            , configMonoidJobs = jobs
-            , configMonoidExtraIncludeDirs = includes
-            , configMonoidExtraLibDirs = libs
-            , configMonoidCustomPreprocessorExts = preprocs
-            , configMonoidOverrideGccPath = overrideGccPath
-            , configMonoidOverrideHpack = overrideHpack
-            , configMonoidSkipMsys = skipMsys
-            , configMonoidLocalBinPath = localBin
-            , configMonoidSetupInfoLocations = setupInfoLocations
-            , configMonoidModifyCodePage = modifyCodePage
-            , configMonoidAllowDifferentUser = allowDifferentUser
-            , configMonoidDumpLogs = dumpLogs
-            , configMonoidColorWhen = colorWhen
-            , configMonoidSnapshotLocation = snapLoc
-            , configMonoidNoRunCompile = noRunCompile
-            })
-    <$> optionalFirst (absDirOption
-            ( long stackRootOptionName
-            <> metavar (map toUpper stackRootOptionName)
-            <> help ("Absolute path to the global Stack root directory " ++
-                     "(Overrides any STACK_ROOT environment variable)")
-            <> hide
-            ))
-    <*> optionalFirst (option (eitherReader (mapLeft showWorkDirError . parseRelDir))
-            ( long "work-dir"
-            <> metavar "WORK-DIR"
-            <> completer (pathCompleterWith (defaultPathCompleterOpts { pcoAbsolute = False, pcoFileFilter = const False }))
-            <> help ("Relative path of work directory " ++
-                     "(Overrides any STACK_WORK environment variable, default is '.stack-work')")
-            <> hide
-            ))
-    <*> buildOptsMonoidParser hide0
-    <*> dockerOptsParser True
-    <*> nixOptsParser True
-    <*> firstBoolFlagsNoDefault
-            "system-ghc"
-            "using the system installed GHC (on the PATH) if it is available and its version matches. Disabled by default."
-            hide
-    <*> firstBoolFlagsTrue
-            "install-ghc"
-            "downloading and installing GHC if necessary (can be done manually \
-            \with 'stack setup')"
-            hide
-    <*> optionalFirst (strOption
-            ( long "arch"
-           <> metavar "ARCH"
-           <> help "System architecture, e.g. i386, x86_64"
-           <> hide
-            ))
-    <*> optionalFirst (ghcVariantParser (hide0 /= OuterGlobalOpts))
-    <*> optionalFirst (ghcBuildParser (hide0 /= OuterGlobalOpts))
-    <*> optionalFirst (option auto
-            ( long "jobs"
-           <> short 'j'
-           <> metavar "JOBS"
-           <> help "Number of concurrent jobs to run"
-           <> hide
-            ))
-    <*> many ((currentDir FilePath.</>) <$> strOption
-            ( long "extra-include-dirs"
-           <> metavar "DIR"
-           <> completer dirCompleter
-           <> help "Extra directories to check for C header files"
-           <> hide
-            ))
-    <*> many ((currentDir FilePath.</>) <$> strOption
-            ( long "extra-lib-dirs"
-           <> metavar "DIR"
-           <> completer dirCompleter
-           <> help "Extra directories to check for libraries"
-           <> hide
-            ))
-    <*> many (strOption
-            ( long "custom-preprocessor-extensions"
-           <> metavar "EXT"
-           <> help "Extensions used for custom preprocessors"
-           <> hide
-            ))
-    <*> optionalFirst (absFileOption
-             ( long "with-gcc"
-            <> metavar "PATH-TO-GCC"
-            <> help "Use gcc found at PATH-TO-GCC"
-            <> hide
-             ))
-    <*> optionalFirst (strOption
-             ( long "with-hpack"
-            <> metavar "HPACK"
-            <> help "Use HPACK executable (overrides bundled Hpack)"
-            <> hide
-             ))
-    <*> firstBoolFlagsFalse
-            "skip-ghc-check"
-            "skipping the GHC version and architecture check"
-            hide
-    <*> firstBoolFlagsFalse
-            "skip-msys"
-            "skipping the local MSYS installation (Windows only)"
-            hide
-    <*> optionalFirst ((currentDir FilePath.</>) <$> strOption
-             ( long "local-bin-path"
-            <> metavar "DIR"
-            <> completer dirCompleter
-            <> help "Install binaries to DIR"
-            <> hide
-             ))
-    <*> many (
-        strOption
-            ( long "setup-info-yaml"
-           <> help "Alternate URL or relative / absolute path for Stack dependencies"
-           <> metavar "URL" ))
-    <*> firstBoolFlagsTrue
-            "modify-code-page"
-            "setting the codepage to support UTF-8 (Windows only)"
-            hide
-    <*> firstBoolFlagsNoDefault
-            "allow-different-user"
-            ("permission for users other than the owner of the Stack root " ++
-                "directory to use a Stack installation (POSIX only) " ++
-                "(default: true inside Docker, otherwise false)")
-            hide
-    <*> fmap toDumpLogs
-            (firstBoolFlagsNoDefault
-             "dump-logs"
-             "dump the build output logs for local packages to the console \
-             \(default: dump warning logs)"
-             hide)
-    <*> optionalFirst (option readColorWhen
-             ( long "color"
-            <> long "colour"
-            <> metavar "WHEN"
-            <> completeWith ["always", "never", "auto"]
-            <> help "Specify when to use color in output; WHEN is 'always', \
-                    \'never', or 'auto'. On Windows versions before Windows \
-                    \10, for terminals that do not support color codes, the \
-                    \default is 'never'; color may work on terminals that \
-                    \support color codes"
-            <> hide
-             ))
-    <*> optionalFirst (strOption
-            ( long "snapshot-location-base"
-           <> help "The base location of LTS/Nightly snapshots"
-           <> metavar "URL"
-            ))
-    <*> firstBoolFlagsFalse
-            "script-no-run-compile"
-            "the use of options `--no-run --compile` with `stack script`"
-            hide
-  where
-    hide = hideMods (hide0 /= OuterGlobalOpts)
-    toDumpLogs (First (Just True)) = First (Just DumpAllLogs)
-    toDumpLogs (First (Just False)) = First (Just DumpNoLogs)
-    toDumpLogs (First Nothing) = First Nothing
-    showWorkDirError err = show err ++
-        "\nNote that --work-dir must be a relative child directory, because work-dirs outside of the package are not supported by Cabal." ++
-        "\nSee https://github.com/commercialhaskell/stack/issues/2954"
+  ( \stackRoot workDir buildOpts dockerOpts nixOpts systemGHC installGHC arch
+     ghcVariant ghcBuild jobs includes libs preprocs overrideGccPath overrideHpack
+     skipGHCCheck skipMsys localBin setupInfoLocations modifyCodePage
+     allowDifferentUser dumpLogs colorWhen snapLoc noRunCompile -> mempty
+       { configMonoidStackRoot = stackRoot
+       , configMonoidWorkDir = workDir
+       , configMonoidBuildOpts = buildOpts
+       , configMonoidDockerOpts = dockerOpts
+       , configMonoidNixOpts = nixOpts
+       , configMonoidSystemGHC = systemGHC
+       , configMonoidInstallGHC = installGHC
+       , configMonoidSkipGHCCheck = skipGHCCheck
+       , configMonoidArch = arch
+       , configMonoidGHCVariant = ghcVariant
+       , configMonoidGHCBuild = ghcBuild
+       , configMonoidJobs = jobs
+       , configMonoidExtraIncludeDirs = includes
+       , configMonoidExtraLibDirs = libs
+       , configMonoidCustomPreprocessorExts = preprocs
+       , configMonoidOverrideGccPath = overrideGccPath
+       , configMonoidOverrideHpack = overrideHpack
+       , configMonoidSkipMsys = skipMsys
+       , configMonoidLocalBinPath = localBin
+       , configMonoidSetupInfoLocations = setupInfoLocations
+       , configMonoidModifyCodePage = modifyCodePage
+       , configMonoidAllowDifferentUser = allowDifferentUser
+       , configMonoidDumpLogs = dumpLogs
+       , configMonoidColorWhen = colorWhen
+       , configMonoidSnapshotLocation = snapLoc
+       , configMonoidNoRunCompile = noRunCompile
+       }
+  )
+  <$> optionalFirst (absDirOption
+        ( long stackRootOptionName
+        <> metavar (map toUpper stackRootOptionName)
+        <> help "Absolute path to the global Stack root directory (Overrides \
+                \any STACK_ROOT environment variable)"
+        <> hide
+        ))
+  <*> optionalFirst (option (eitherReader (mapLeft showWorkDirError . parseRelDir))
+        ( long "work-dir"
+        <> metavar "WORK-DIR"
+        <> completer
+             ( pathCompleterWith
+               ( defaultPathCompleterOpts
+                   { pcoAbsolute = False, pcoFileFilter = const False }
+               )
+             )
+        <> help "Relative path of work directory (Overrides any STACK_WORK \
+                \environment variable, default is '.stack-work')"
+        <> hide
+        ))
+  <*> buildOptsMonoidParser hide0
+  <*> dockerOptsParser True
+  <*> nixOptsParser True
+  <*> firstBoolFlagsNoDefault
+        "system-ghc"
+        "using the system installed GHC (on the PATH) if it is available and \
+        \its version matches. Disabled by default."
+        hide
+  <*> firstBoolFlagsTrue
+        "install-ghc"
+        "downloading and installing GHC if necessary (can be done manually \
+        \with 'stack setup')"
+        hide
+  <*> optionalFirst (strOption
+        (  long "arch"
+        <> metavar "ARCH"
+        <> help "System architecture, e.g. i386, x86_64"
+        <> hide
+        ))
+  <*> optionalFirst (ghcVariantParser (hide0 /= OuterGlobalOpts))
+  <*> optionalFirst (ghcBuildParser (hide0 /= OuterGlobalOpts))
+  <*> optionalFirst (option auto
+        (  long "jobs"
+        <> short 'j'
+        <> metavar "JOBS"
+        <> help "Number of concurrent jobs to run"
+        <> hide
+        ))
+  <*> many ((currentDir FilePath.</>) <$> strOption
+        (  long "extra-include-dirs"
+        <> metavar "DIR"
+        <> completer dirCompleter
+        <> help "Extra directories to check for C header files"
+        <> hide
+        ))
+  <*> many ((currentDir FilePath.</>) <$> strOption
+        (  long "extra-lib-dirs"
+        <> metavar "DIR"
+        <> completer dirCompleter
+        <> help "Extra directories to check for libraries"
+        <> hide
+        ))
+  <*> many (strOption
+        (  long "custom-preprocessor-extensions"
+        <> metavar "EXT"
+        <> help "Extensions used for custom preprocessors"
+        <> hide
+        ))
+  <*> optionalFirst (absFileOption
+        (  long "with-gcc"
+        <> metavar "PATH-TO-GCC"
+        <> help "Use gcc found at PATH-TO-GCC"
+        <> hide
+        ))
+  <*> optionalFirst (strOption
+        (  long "with-hpack"
+        <> metavar "HPACK"
+        <> help "Use HPACK executable (overrides bundled Hpack)"
+        <> hide
+        ))
+  <*> firstBoolFlagsFalse
+        "skip-ghc-check"
+        "skipping the GHC version and architecture check"
+        hide
+  <*> firstBoolFlagsFalse
+        "skip-msys"
+        "skipping the local MSYS installation (Windows only)"
+        hide
+  <*> optionalFirst ((currentDir FilePath.</>) <$> strOption
+        ( long "local-bin-path"
+        <> metavar "DIR"
+        <> completer dirCompleter
+        <> help "Install binaries to DIR"
+        <> hide
+        ))
+  <*> many (strOption
+        (  long "setup-info-yaml"
+        <> help "Alternate URL or relative / absolute path for Stack \
+                \dependencies"
+        <> metavar "URL"
+        ))
+  <*> firstBoolFlagsTrue
+        "modify-code-page"
+        "setting the codepage to support UTF-8 (Windows only)"
+        hide
+  <*> firstBoolFlagsNoDefault
+        "allow-different-user"
+        "permission for users other than the owner of the Stack root directory \
+        \to use a Stack installation (POSIX only) (default: true inside \
+        \Docker, otherwise false)"
+        hide
+  <*> fmap toDumpLogs (firstBoolFlagsNoDefault
+        "dump-logs"
+        "dump the build output logs for local packages to the console \
+        \(default: dump warning logs)"
+        hide)
+  <*> optionalFirst (option readColorWhen
+        (  long "color"
+        <> long "colour"
+        <> metavar "WHEN"
+        <> completeWith ["always", "never", "auto"]
+        <> help "Specify when to use color in output; WHEN is 'always', \
+                \'never', or 'auto'. On Windows versions before Windows \
+                \10, for terminals that do not support color codes, the \
+                \default is 'never'; color may work on terminals that \
+                \support color codes"
+        <> hide
+        ))
+  <*> optionalFirst (strOption
+        (  long "snapshot-location-base"
+        <> help "The base location of LTS/Nightly snapshots"
+        <> metavar "URL"
+        ))
+  <*> firstBoolFlagsFalse
+        "script-no-run-compile"
+        "the use of options `--no-run --compile` with `stack script`"
+        hide
+ where
+  hide = hideMods (hide0 /= OuterGlobalOpts)
+  toDumpLogs (First (Just True)) = First (Just DumpAllLogs)
+  toDumpLogs (First (Just False)) = First (Just DumpNoLogs)
+  toDumpLogs (First Nothing) = First Nothing
+  showWorkDirError err = show err ++
+    "\nNote that --work-dir must be a relative child directory, because \
+    \work-dirs outside of the package are not supported by Cabal." ++
+    "\nSee https://github.com/commercialhaskell/stack/issues/2954"
--- src/Stack/Options/DockerParser.hs
+++ src/Stack/Options/DockerParser.hs
@@ -1,108 +1,151 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 
-module Stack.Options.DockerParser where
+module Stack.Options.DockerParser
+  ( dockerOptsParser
+  ) where
 
 import           Data.List ( intercalate )
 import qualified Data.Text as T
 import           Distribution.Version ( anyVersion )
 import           Options.Applicative
-import           Options.Applicative.Args
+                   ( Parser, auto, completer, help, listCompleter, long, metavar
+                   , option, str, value
+                   )
+import           Options.Applicative.Args ( argsOption )
 import           Options.Applicative.Builder.Extra
-import           Stack.Docker
+                   ( dirCompleter, eitherReader', fileCompleter
+                   , firstBoolFlagsFalse, firstBoolFlagsNoDefault
+                   , firstBoolFlagsTrue, optionalFirst
+                   )
+import           Stack.Docker ( dockerCmdName )
 import           Stack.Prelude
-import           Stack.Options.Utils
-import           Stack.Types.Version
+import           Stack.Options.Utils ( hideMods )
+import           Stack.Types.Version ( IntersectingVersionRange (..) )
 import           Stack.Types.Docker
+                   ( DockerMonoidRepoOrImage (..), DockerOptsMonoid (..)
+                   , dockerAutoPullArgName, dockerImageArgName
+                   , dockerContainerNameArgName, dockerDetachArgName
+                   , dockerEnvArgName, dockerPersistArgName
+                   , dockerRegistryLoginArgName, dockerRegistryPasswordArgName
+                   , dockerRegistryUsernameArgName, dockerRepoArgName
+                   , dockerRunArgsArgName, dockerMountArgName
+                   , dockerMountModeArgName, dockerNetworkArgName
+                   , dockerSetUserArgName, dockerStackExeArgName
+                   , dockerStackExeDownloadVal, dockerStackExeHostVal
+                   , dockerStackExeImageVal, parseDockerStackExe
+                   )
 
 -- | Options parser configuration for Docker.
 dockerOptsParser :: Bool -> Parser DockerOptsMonoid
-dockerOptsParser hide0 =
-    DockerOptsMonoid (Any False)
-    <$> firstBoolFlagsNoDefault
-                       dockerCmdName
-                       "using a Docker container. --docker implies 'system-ghc: true'"
-                       hide
-    <*> fmap First
-           (Just . DockerMonoidRepo <$> option str (long (dockerOptName dockerRepoArgName) <>
-                                                     hide <>
-                                                     metavar "NAME" <>
-                                                     help "Docker repository name") <|>
-             Just . DockerMonoidImage <$> option str (long (dockerOptName dockerImageArgName) <>
-                                                      hide <>
-                                                      metavar "IMAGE" <>
-                                                      help "Exact Docker image ID (overrides docker-repo)") <|>
-         pure Nothing)
-    <*> firstBoolFlagsNoDefault
-                       (dockerOptName dockerRegistryLoginArgName)
-                       "registry requires login"
-                       hide
-    <*> firstStrOption (long (dockerOptName dockerRegistryUsernameArgName) <>
-                        hide <>
-                        metavar "USERNAME" <>
-                        help "Docker registry username")
-    <*> firstStrOption (long (dockerOptName dockerRegistryPasswordArgName) <>
-                        hide <>
-                        metavar "PASSWORD" <>
-                        help "Docker registry password")
-    <*> firstBoolFlagsTrue
-                       (dockerOptName dockerAutoPullArgName)
-                       "automatic pulling latest version of image"
-                       hide
-    <*> firstBoolFlagsFalse
-                       (dockerOptName dockerDetachArgName)
-                       "running a detached Docker container"
-                       hide
-    <*> firstBoolFlagsFalse
-                       (dockerOptName dockerPersistArgName)
-                       "not deleting container after it exits"
-                       hide
-    <*> firstStrOption (long (dockerOptName dockerContainerNameArgName) <>
-                        hide <>
-                        metavar "NAME" <>
-                        help "Docker container name")
-    <*> firstStrOption (long (dockerOptName dockerNetworkArgName) <>
-                        hide <>
-                        metavar "NETWORK" <>
-                        help "Docker network")
-    <*> argsOption (long (dockerOptName dockerRunArgsArgName) <>
-                    hide <>
-                    value [] <>
-                    metavar "'ARG1 [ARG2 ...]'" <>
-                    help "Additional options to pass to 'docker run'")
-    <*> many (option auto (long (dockerOptName dockerMountArgName) <>
-                           hide <>
-                           metavar "(PATH | HOST-PATH:CONTAINER-PATH)" <>
-                           completer dirCompleter <>
-                           help ("Mount volumes from host in container " ++
-                                 "(can be specified multiple times)")))
-    <*> firstStrOption (long (dockerOptName dockerMountModeArgName) <>
-                        hide <>
-                        metavar "SUFFIX" <>
-                        help "Volume mount mode suffix")
-    <*> many (option str (long (dockerOptName dockerEnvArgName) <>
-                                hide <>
-                                metavar "NAME=VALUE" <>
-                                help ("Set environment variable in container " ++
-                                      "(can be specified multiple times)")))
-    <*> optionalFirst (option (eitherReader' parseDockerStackExe)
-            (let specialOpts =
-                     [ dockerStackExeDownloadVal
-                     , dockerStackExeHostVal
-                     , dockerStackExeImageVal
-                     ] in
-             long(dockerOptName dockerStackExeArgName) <>
-             hide <>
-             metavar (intercalate "|" (specialOpts ++ ["PATH"])) <>
-             completer (listCompleter specialOpts <> fileCompleter) <>
-             help (concat [ "Location of "
+dockerOptsParser hide0 = DockerOptsMonoid (Any False)
+  <$> firstBoolFlagsNoDefault
+        dockerCmdName
+        "using a Docker container. --docker implies 'system-ghc: true'"
+        hide
+  <*> fmap First
+        (   Just . DockerMonoidRepo <$> option str
+              (  long (dockerOptName dockerRepoArgName)
+              <> hide
+              <> metavar "NAME"
+              <> help "Docker repository name"
+              )
+        <|> Just . DockerMonoidImage <$> option str
+              (  long (dockerOptName dockerImageArgName)
+              <> hide
+              <> metavar "IMAGE"
+              <> help "Exact Docker image ID (overrides docker-repo)"
+              )
+        <|> pure Nothing
+        )
+  <*> firstBoolFlagsNoDefault
+        (dockerOptName dockerRegistryLoginArgName)
+        "registry requires login"
+        hide
+  <*> firstStrOption
+        (  long (dockerOptName dockerRegistryUsernameArgName)
+        <> hide
+        <> metavar "USERNAME"
+        <> help "Docker registry username"
+        )
+  <*> firstStrOption
+        (  long (dockerOptName dockerRegistryPasswordArgName)
+        <> hide
+        <> metavar "PASSWORD"
+        <> help "Docker registry password"
+        )
+  <*> firstBoolFlagsTrue
+        (dockerOptName dockerAutoPullArgName)
+        "automatic pulling latest version of image"
+        hide
+  <*> firstBoolFlagsFalse
+        (dockerOptName dockerDetachArgName)
+        "running a detached Docker container"
+        hide
+  <*> firstBoolFlagsFalse
+        (dockerOptName dockerPersistArgName)
+        "not deleting container after it exits"
+        hide
+  <*> firstStrOption
+        (  long (dockerOptName dockerContainerNameArgName)
+        <> hide
+        <> metavar "NAME"
+        <> help "Docker container name"
+        )
+  <*> firstStrOption
+        (  long (dockerOptName dockerNetworkArgName)
+        <> hide
+        <> metavar "NETWORK"
+        <> help "Docker network"
+        )
+  <*> argsOption
+        (  long (dockerOptName dockerRunArgsArgName)
+        <> hide
+        <> value []
+        <> metavar "'ARG1 [ARG2 ...]'"
+        <> help "Additional options to pass to 'docker run'")
+  <*> many (option auto
+        (  long (dockerOptName dockerMountArgName)
+        <> hide
+        <> metavar "(PATH | HOST-PATH:CONTAINER-PATH)"
+        <> completer dirCompleter
+        <> help "Mount volumes from host in container (can be specified \
+                \multiple times)"
+        ))
+  <*> firstStrOption
+        (  long (dockerOptName dockerMountModeArgName)
+        <> hide
+        <> metavar "SUFFIX"
+        <> help "Volume mount mode suffix"
+        )
+  <*> many (option str
+        (  long (dockerOptName dockerEnvArgName)
+        <> hide
+        <> metavar "NAME=VALUE"
+        <> help "Set environment variable in container (can be specified \
+                \multiple times)"
+        ))
+  <*> optionalFirst (option (eitherReader' parseDockerStackExe)
+        ( let specialOpts = [ dockerStackExeDownloadVal
+                            , dockerStackExeHostVal
+                            , dockerStackExeImageVal
+                            ]
+          in     long (dockerOptName dockerStackExeArgName)
+              <> hide
+              <> metavar (intercalate "|" (specialOpts ++ ["PATH"]))
+              <> completer (listCompleter specialOpts <> fileCompleter)
+              <> help ( concat
+                          [ "Location of "
                           , stackProgName
-                          , " executable used in container" ])))
-    <*> firstBoolFlagsNoDefault
-                       (dockerOptName dockerSetUserArgName)
-                       "setting user in container to match host"
-                       hide
-    <*> pure (IntersectingVersionRange anyVersion)
-  where
-    dockerOptName optName = dockerCmdName ++ "-" ++ T.unpack optName
-    firstStrOption = optionalFirst . option str
-    hide = hideMods hide0
+                          , " executable used in container"
+                          ]
+                      )
+        ))
+  <*> firstBoolFlagsNoDefault
+        (dockerOptName dockerSetUserArgName)
+        "setting user in container to match host"
+        hide
+  <*> pure (IntersectingVersionRange anyVersion)
+ where
+  dockerOptName optName = dockerCmdName ++ "-" ++ T.unpack optName
+  firstStrOption = optionalFirst . option str
+  hide = hideMods hide0
