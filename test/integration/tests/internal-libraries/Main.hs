805e1ef4ee0ea9c03254b82cd6373f37dcbcabb8
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2023-05-08 17:29:21 +0100

    Bump `internal-libraries` test to GHC 9.4.5.

--- test/integration/tests/internal-libraries/Main.hs
+++ test/integration/tests/internal-libraries/Main.hs
@@ -1,15 +1,9 @@
-import Control.Monad ( unless )
 import StackTest
 
 main :: IO ()
--- See https://github.com/haskell/cabal/issues/7763. On macOS (Apple silicon),
--- fails with Cabal error:
--- "Cabal-simple_SvXsv1f__3.2.1.0_ghc-8.10.7: Cannot build some foreign libraries:
--- Building foreign libraries is currently only supported on OSX, Linux and
--- Windows"
-main = unless ( isAarch64 && isMacOSX ) $ do
+main = do
   -- The '--install-ghc' flag is passed here, because etc/scripts/release.hs
   -- passes `--no-install-ghc` when `--alpine` is passed to its 'check'
   -- command.
-  stack ["--install-ghc", "setup"] -- See stack.yaml; using GHC 8.10.7
+  stack ["--install-ghc", "setup"] -- See stack.yaml; using GHC 9.4.5
   stack ["build"]
