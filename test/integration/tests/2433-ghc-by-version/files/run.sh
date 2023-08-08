8d15d0ab241dca6d45be8b4cf8501581a1db0fe5
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2023-08-08 22:54:55 +0100

    Re #6160 Move Stack on to GHC 9.2.8

--- test/integration/tests/2433-ghc-by-version/files/run.sh
+++ test/integration/tests/2433-ghc-by-version/files/run.sh
@@ -2,10 +2,10 @@
 
 set -exuo pipefail
 
-export PATH=$(pwd)/fake-path:$("$STACK_EXE" path --resolver ghc-9.2.7 --compiler-bin):$PATH
+export PATH=$(pwd)/fake-path:$("$STACK_EXE" path --resolver ghc-9.2.8 --compiler-bin):$PATH
 export STACK_ROOT=$(pwd)/fake-root
 
 which ghc
 
-"$STACK_EXE" --system-ghc --no-install-ghc --resolver ghc-9.2.7 ghc -- --info
-"$STACK_EXE" --system-ghc --no-install-ghc --resolver ghc-9.2.7 runghc foo.hs
+"$STACK_EXE" --system-ghc --no-install-ghc --resolver ghc-9.2.8 ghc -- --info
+"$STACK_EXE" --system-ghc --no-install-ghc --resolver ghc-9.2.8 runghc foo.hs
