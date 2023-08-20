d1ab996e91353362273ac1e21a620aae9cd00a4c
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2023-08-20 20:56:47 +0100

    Revise integration test `3685-config-yaml-for-allow-newer`

--- test/integration/tests/3685-config-yaml-for-allow-newer/Main.hs
+++ test/integration/tests/3685-config-yaml-for-allow-newer/Main.hs
@@ -3,22 +3,16 @@ import Data.List (isInfixOf)
 import StackTest
 import System.Directory
 
+planRecommendation :: String
+planRecommendation = "To ignore all version constraints"
+
 main :: IO ()
 main = do
   removeFileIgnore "stack.yaml"
   stack ["init", defaultResolverArg]
-  (_, stdErr) <- stackStderr ["install", "intero-0.1.23"]
-  -- here we check stderr for 'allow-newer: true' and
-  -- config.yaml sitting either on the same line or on
-  -- two consecutive lines
-  let errLines = lines stdErr
-      hasNewer l = "allow-newer: true" `isInfixOf` l
-      withNewer = map hasNewer errLines
-      userConfig = "config.yaml"
-      hasConfigForAllowNewer prevNewer l =
-         (prevNewer || hasNewer l) &&
-        userConfig `isInfixOf` l
-      hasProperLines =
-        or $ zipWith hasConfigForAllowNewer (False:withNewer) errLines
-  unless hasProperLines $
-    error $ "Not stderr lines with 'allow-newer: true' and " ++ userConfig
+  stackErrStderr ["install", "intero-0.1.23"] (expectMessage planRecommendation)
+
+expectMessage :: String -> String -> IO ()
+expectMessage msg stderr = do
+  unless (words msg `isInfixOf` words stderr)
+         (error $ "Expected a recommendation: \n" ++ show msg)
