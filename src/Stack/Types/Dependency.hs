f138991082a8e73c092ea0c2d118ef426d221528
theophilebatoz <theophile.batoz@gmail.com>
2023-11-15 09:29:26 +0000

    fix: remove unused and buggy instances of semigroup for dependency

--- src/Stack/Types/Dependency.hs
+++ src/Stack/Types/Dependency.hs
@@ -10,18 +10,14 @@ module Stack.Types.Dependency
 import qualified Distribution.PackageDescription as Cabal
 import           Distribution.Types.VersionRange ( VersionRange )
 import           Stack.Prelude
-import           Stack.Types.Version ( intersectVersionRanges )
 
 -- | The value for a map from dependency name. This contains both the version
--- range and the type of dependency, and provides a semigroup instance.
+-- range and the type of dependency.
 data DepValue = DepValue
   { dvVersionRange :: !VersionRange
   , dvType :: !DepType
   }
   deriving (Show, Typeable)
-
-instance Semigroup DepValue where
-  DepValue a x <> DepValue b y = DepValue (intersectVersionRanges a b) (x <> y)
 
 -- | Is this package being used as a library, or just as a build tool? If the
 -- former, we need to ensure that a library actually exists. See
@@ -30,10 +26,6 @@ data DepType
   = AsLibrary
   | AsBuildTool
   deriving (Eq, Show)
-
-instance Semigroup DepType where
-  AsLibrary <> _ = AsLibrary
-  AsBuildTool <> x = x
 
 cabalToStackDep :: Cabal.Dependency -> DepValue
 cabalToStackDep (Cabal.Dependency _ verRange _libNameSet) =
