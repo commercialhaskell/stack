da8bc4032b4d5ca7b846ba8ee54a84dc6a9a2f92
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2024-12-10 21:49:32 +0000

    Re #6666 Re-activate test haddock-options

--- tests/integration/tests/haddock-options/Main.hs
+++ tests/integration/tests/haddock-options/Main.hs
@@ -1,17 +1,24 @@
-import StackTest
+import           Control.Monad ( unless )
+import           Data.List ( isInfixOf )
+import           StackTest
 
 main :: IO ()
 main = do
-    removeDirIgnore ".stack-work"
+  removeDirIgnore ".stack-work"
 
--- Disabling the test with GHC 9.8.4 because, unlike earlier versions, it fails
--- to fail, despite the Haddock options working as expected.
---
---  -- Fails to work because BAR is defined here and FOO in stack file
---  stackErr ["haddock", "--haddock-arguments", "--optghc=-DBAR"]
-    stack ["clean"]
-    -- Works just fine, test #3099 while at it.
-    stack ["haddock", "--no-haddock-hyperlink-source"]
-    stack ["clean"]
-    -- Fails to work because we have bad argument
-    stackErr ["haddock", "--haddock-arguments", "--stack_it_badhaddockargument"]
+  -- BAR is defined here and FOO in stack.yaml file
+  stackCheckStderr ["haddock", "--haddock-arguments", "--optghc=-DBAR"] $ \s ->
+    unless (errorMsg `isInfixOf` s) $
+      error "FOO and BAR not both defined"
+  stack ["clean"]
+  -- Works just fine, test #3099 while at it.
+  stack ["haddock", "--no-haddock-hyperlink-source"]
+  stack ["clean"]
+  -- Fails to work because we have bad argument
+  stackErr ["haddock", "--haddock-arguments", "--stack_it_badhaddockargument"]
+
+-- The error message differs by operating system
+errorMsg :: String
+errorMsg = if isLinux
+  then "error: #error FOO and BAR is defined"
+  else "error: FOO and BAR is defined"
