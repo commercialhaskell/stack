b000c57e912353f23a11c096b4bdba5cefa2ffbe
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2025-04-21 14:29:50 +0100

    Re #6720 Output information about the ld linker

--- tests/integration/tests/4783-doctest-deps/Main.hs
+++ tests/integration/tests/4783-doctest-deps/Main.hs
@@ -3,5 +3,7 @@ import StackTest
 main :: IO ()
 main = do
   stackCleanFull
+  stack ["exec", "--", "which", "ld"]
+  stack ["exec", "--", "ld", "-v"]
   stack ["--verbose", "build", "acme-dont-copy"]
   stack ["--verbose", "test"]
