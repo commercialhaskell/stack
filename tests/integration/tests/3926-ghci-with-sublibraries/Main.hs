07c22238f67568389de50b54b425e462684bee56
Max Ulidtko <ulidtko@gmail.com>
2025-05-17 16:37:57 +0200

    cleanup: undo tests workarounds for msys2-20230526
    Resolves #6170

--- tests/integration/tests/3926-ghci-with-sublibraries/Main.hs
+++ tests/integration/tests/3926-ghci-with-sublibraries/Main.hs
@@ -11,13 +11,10 @@ main = do
   copy "src-internal/Internal.v1" "src-internal/Internal.hs"
   stack ["build"] -- need a build before ghci at the moment, see #4148
   stackRepl [] $ do
-    -- The command must be issued before searching the output for the next prompt,
-    -- otherwise, on Windows from msys2-20230526, `stack repl` encounters a EOF
-    -- and terminates gracefully.
-    replCommand ":main"
     liftIO $ putStrLn "Awaiting prompt..."
     nextPrompt
     liftIO $ putStrLn "Initial prompt received"
+    replCommand ":main"
     line <- replGetLine
     let expected = "hello world"
     when (line /= expected) $
