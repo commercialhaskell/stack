003de59162ac544f3c8add217d16535c97019103
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2025-07-15 00:59:24 +0100

    Fix test cyclic-test-deps

--- tests/integration/tests/cyclic-test-deps/Main.hs
+++ tests/integration/tests/cyclic-test-deps/Main.hs
@@ -2,9 +2,9 @@ import StackTest
 
 main :: IO ()
 main = do
-    removeDirIgnore "text-2.1"
-    stack ["unpack", "text-2.1"]
-    stack ["unpack", "QuickCheck-2.14.3"]
+    removeDirIgnore "text-2.1.2"
+    stack ["unpack", "text-2.1.2"]
+    stack ["unpack", "QuickCheck-2.15.0.1"]
     removeFileIgnore "stack.yaml"
     stack ["init", defaultSnapshotArg]
     stack ["test", "--dry-run"]
