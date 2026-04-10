42b3974ea3ba07ad41bee80b2105a2d69758d2b6
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2026-04-10 23:05:09 +0100

    Clear HLint hints

--- src/Stack/Package.hs
+++ src/Stack/Package.hs
@@ -945,5 +945,6 @@ packageIsIndefinite :: Package -> Bool
 packageIsIndefinite pkg = any hasSignatures allLibs
  where
   hasSignatures lib = not (null lib.signatures)
-  allLibs = maybe [] (:[]) pkg.library
-         ++ toList pkg.subLibraries
+  allLibs =
+       maybeToList pkg.library
+    ++ toList pkg.subLibraries
