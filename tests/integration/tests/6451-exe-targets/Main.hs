b882989a2c6449a8dc56a84d41db45ae10bb4d8c
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2026-04-25 22:07:55 +0100

    Fix #6882 Update 3229-exe-targets integration test

---
+++ tests/integration/tests/6451-exe-targets/Main.hs
@@ -0,0 +1,22 @@
+-- | Stack should build only those executables requested by the program
+-- arguments.
+--
+-- Issue: https://github.com/commercialhaskell/stack/issues/3229 is no longer
+-- applicable.
+
+module Main where
+
+import           Control.Monad ( when )
+import           Data.List ( isInfixOf )
+import           StackTest
+
+main :: IO ()
+main = do
+  stackCheckStderr
+    ["build", ":alpha"]
+    (rejectMessage (unlines ["Installing executable beta in"]))
+
+rejectMessage :: String -> String -> IO ()
+rejectMessage msg stderr =
+    when (msg `isInfixOf` stderr)
+            (error $ "Did not expect message here: \n" ++ show msg)
