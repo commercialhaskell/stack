7ec45a75893e23819dc657e1101195d9e20ce567
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2026-07-04 14:52:15 +0100

    Fixup: Allow for whitespace in backpack-undeclared-signature-warning test

--- tests/integration/tests/backpack-undeclared-signature-warning/Main.hs
+++ tests/integration/tests/backpack-undeclared-signature-warning/Main.hs
@@ -1,23 +1,25 @@
--- Stack should explain likely Backpack signature mistakes with a Backpack
--- warning, not the custom-preprocessor warning.
+-- | Stack explains likely Backpack signature mistakes with a useful warning,
+-- and not an inappropriate warning about custom preprocessors.
+--
+-- See: https://github.com/commercialhaskell/stack/issues/6937
 
-import Control.Monad ( unless, when )
-import Data.List ( isInfixOf )
-import StackTest
+import           Control.Monad ( unless, when )
+import           Data.List ( isInfixOf )
+import           StackTest
 
 main :: IO ()
 main =
   stackErrStderr ["build"] $ \err -> do
-    expect err "Found Backpack signature file for Cabal entry"
-    expect err "Logger"
-    expect err "not listed in the component's"
-    expect err "signatures"
+    expectMessage "Found Backpack signature file for Cabal entry" err
+    expectMessage "Logger" err
+    expectMessage "not listed in the component's" err
+    expectMessage "signatures" err
     when ("custom-preprocessor-extensions" `isInfixOf` err) $
       error $
-           "Expected no custom-preprocessor warning for Logger.hsig, got: "
-        ++ show err
+           "Expected no custom-preprocessor warning for Logger.hsig, got:\n\n"
+        ++ err
 
-expect :: String -> String -> IO ()
-expect err msg =
-  unless (msg `isInfixOf` err) $
-    error $ "Expected " ++ show msg ++ " in stderr, got: " ++ show err
+expectMessage :: String -> String -> IO ()
+expectMessage msg stderr = do
+  unless (words msg `isInfixOf` words stderr) $
+    error $ "Expected " ++ show msg ++ " in stderr, got:\n\n" ++ stderr
